e3278f715a68b36b56d34f2da29a09aa42fe09ef
Hiru1003 <hirumiperera10@gmail.com>
2023-11-05 10:32:24 +0530

    pages for navigation bar
    home page, diary page, profile page, chat page and report page with nav bar dependancies

---
+++ myapp/lib/pages/chat.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class chat_page extends StatefulWidget{
+  
+    const chat_page ({
+    super.key,});
+
+  @override
+  State<chat_page > createState()  => _chat_pageState();
+}
+
+class _chat_pageState extends State<chat_page >{
+
+
+final nameController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold( 
+    backgroundColor: const Color.fromARGB(251, 237, 251, 223),
+      body: SingleChildScrollView(
+      )
+);
+}
+}
