a419174fb9c8fad9f1ea4928662851e7d5618e96
Hiru1003 <hirumiperera10@gmail.com>
2023-12-04 18:57:03 +0530

    meditation methods page

---
+++ myapp/lib/pages/guidedvisualization.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class guidedvis_page extends StatefulWidget{
+  
+    const guidedvis_page({
+    super.key,});
+
+  @override
+  State<guidedvis_page> createState()  => _guidedvis_pageState();
+}
+
+class _guidedvis_pageState extends State<guidedvis_page>{
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold();
+  }
+}
---
+++ myapp/lib/pages/walkingmed.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class walkingmed_page extends StatefulWidget{
+  
+    const walkingmed_page({
+    super.key,});
+
+  @override
+  State<walkingmed_page> createState()  => _walkingmed_pageState();
+}
+
+class _walkingmed_pageState extends State<walkingmed_page>{
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold();
+  }
+}
