2dabcc5f6b8277fe429e076404b740032b2a8e4b
Hiru1003 <hirumiperera10@gmail.com>
2023-12-15 21:27:40 +0530

    add breathawareness step 1

---
+++ myapp/lib/pages/breathawarenessstep2.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class breathawarenessstep2_page extends StatefulWidget{
+  
+    const breathawarenessstep2_page({
+    super.key,});
+
+  @override
+  State<breathawarenessstep2_page> createState()  => _breathawarenessstep2_pageState();
+}
+
+class _breathawarenessstep2_pageState extends State<breathawarenessstep2_page>{
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold( 
+
+  );
+  }}
