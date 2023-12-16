04c869d51d17246497cefd25466148e5d20588bf
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2023-12-16 12:26:20 +0530

    feat: edit profile

--- myapp/lib/pages/profile.dart
+++ myapp/lib/pages/profile.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
 
-class profile_page extends StatefulWidget{
-  
-    const profile_page ({
-    super.key,});
+class profile_page extends StatefulWidget {
+  const profile_page({
+    super.key,
+  });
 
   @override
-  State<profile_page > createState()  => _profile_pageState();
+  State<profile_page> createState() => _profile_pageState();
 }
 
-class _profile_pageState extends State<profile_page >{
-
-
-final nameController = TextEditingController();
-
+class _profile_pageState extends State<profile_page> {
+  final nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold( 
-    backgroundColor: const Color.fromARGB(251, 237, 251, 223),
-      body: SingleChildScrollView()
-);
-}
+    return const Scaffold(
+        backgroundColor: Color.fromARGB(251, 237, 251, 223),
+        body: SingleChildScrollView());
+  }
 }
