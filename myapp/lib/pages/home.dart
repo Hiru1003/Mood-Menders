8381475376c4b125b3da23003765fc373941f149
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2023-12-16 13:06:12 +0530

    style: edit home_page

--- myapp/lib/pages/home.dart
+++ myapp/lib/pages/home.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
 
-class home_page extends StatefulWidget{
-  
-    const home_page ({
-    super.key,});
+class home_page extends StatefulWidget {
+  const home_page({
+    super.key,
+  });
 
   @override
-  State<home_page > createState()  => _home_pageState();
+  State<home_page> createState() => _home_pageState();
 }
 
-class _home_pageState extends State<home_page >{
-
-
-final nameController = TextEditingController();
-
+class _home_pageState extends State<home_page> {
+  final nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold( 
-    backgroundColor: const Color.fromARGB(251, 237, 251, 223),
-      body: SingleChildScrollView(
-
-)
-);
-}
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(251, 237, 251, 223),
+        body: SingleChildScrollView());
+  }
 }
