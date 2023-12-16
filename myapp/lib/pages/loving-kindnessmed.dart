822dd5e565dd1475f07639cc8ecde2747ef51646
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2023-12-16 13:13:12 +0530

    style: edit lovekindmed_page

--- myapp/lib/pages/loving-kindnessmed.dart
+++ myapp/lib/pages/loving-kindnessmed.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
-class lovekindmed_page extends StatefulWidget{
-  
-    const lovekindmed_page({
-    super.key,});
+class lovekindmed_page extends StatefulWidget {
+  const lovekindmed_page({
+    super.key,
+  });
 
   @override
-  State<lovekindmed_page> createState()  => _lovekindmed_pageState();
+  State<lovekindmed_page> createState() => _lovekindmed_pageState();
 }
 
-class _lovekindmed_pageState extends State<lovekindmed_page>{
+class _lovekindmed_pageState extends State<lovekindmed_page> {
   @override
   Widget build(BuildContext context) {
-  return Scaffold();
+    return Scaffold();
   }
 }
