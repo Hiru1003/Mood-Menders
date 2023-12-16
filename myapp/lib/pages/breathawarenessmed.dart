02a8437a70b03686ee184fa642bd6aaa2663afd2
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2023-12-16 12:27:54 +0530

    feat: edit breathawarenessmed

--- myapp/lib/pages/breathawarenessmed.dart
+++ myapp/lib/pages/breathawarenessmed.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
-class breathmed_page extends StatefulWidget{
-  
-    const breathmed_page({
-    super.key,});
+class breathmed_page extends StatefulWidget {
+  const breathmed_page({
+    super.key,
+  });
 
   @override
-  State<breathmed_page> createState()  => _breathmed_pageState();
+  State<breathmed_page> createState() => _breathmed_pageState();
 }
 
-class _breathmed_pageState extends State<breathmed_page>{
+class _breathmed_pageState extends State<breathmed_page> {
   @override
   Widget build(BuildContext context) {
-  return Scaffold();
+    return Scaffold();
   }
 }
