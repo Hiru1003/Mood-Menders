4e84c7e64c35fd065cb197fa803e2c99b3866037
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-01-11 19:51:41 +0530

    Update main.dart

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -7,6 +7,7 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return const MaterialApp();
