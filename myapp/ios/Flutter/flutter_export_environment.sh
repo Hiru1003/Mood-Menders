62aebbb86e65145c3f52668d4dbd92b40cc650d7
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-04 13:48:50 +0530

    upgrade: flutter version + dependencies

--- myapp/ios/Flutter/flutter_export_environment.sh
+++ myapp/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\HP\flutter_windows_3.13.6-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\HP\OneDrive\Desktop\Flutter\myapp"
+export "FLUTTER_ROOT=C:\flutter install\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\HP\app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
