9101aed4b5eed10a83cc486f715c8f77679f6f45
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-06 22:07:04 +0530

    fix: stage 1 java

--- myapp/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ myapp/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter install\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\HP\app"
+export "FLUTTER_APPLICATION_PATH=C:\Users\HP\OneDrive\Desktop\please\Mood-Menders\myapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
