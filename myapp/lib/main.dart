d3f93d5aaea46ec53b5593e4f8f4148855bbaae0
Hiru1003 <hirumiperera10@gmail.com>
2024-02-12 09:35:04 +0530

    fix: error

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:app/loading_page.dart';
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
- @override
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
