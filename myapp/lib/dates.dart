6cc3d1ba7990da2327c899a7d7bda608eac36576
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-13 14:42:16 +0530

    style: dates

--- myapp/lib/dates.dart
+++ myapp/lib/dates.dart
@@ -13,7 +13,7 @@ class DatesWidget extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: CircleAvatar(
             radius: i == 0 ? 25 : 20, 
-            backgroundColor: const Color.fromARGB(255, 204, 248, 245),
+            backgroundColor: Color.fromARGB(255, 154, 191, 188),
             child: Text(
               date.day.toString(),
               style: TextStyle(
--- myapp/lib/diary.dart
+++ myapp/lib/diary.dart
@@ -109,22 +109,32 @@ class Diary extends StatelessWidget {
                     ),
                   ),
                 ),
-            
-            
               ],
             ),
             DatesWidget(),
             Container(
-          margin: EdgeInsets.symmetric(vertical: 10), // Add some vertical margin
-          width: 300, // Adjust the width as needed
-          height: 0.5, // Adjust the thickness as needed
-          color: const Color.fromARGB(255, 70, 66, 68), // Adjust the color as needed
-        ),
+              margin: EdgeInsets.symmetric(vertical: 10), // Add some vertical margin
+              width: 300, // Adjust the width as needed
+              height: 0.5, // Adjust the thickness as needed
+              color: const Color.fromARGB(255, 70, 66, 68), // Adjust the color as needed
+            ),
+            Container(
+              padding: EdgeInsets.all(10), // Add some padding
+              margin: EdgeInsets.all(10), // Add some margin
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 204, 248, 245), // Set the color of the box
+                border: Border.all(color: Colors.grey), // Set the border color
+                borderRadius: BorderRadius.circular(10), // Set the border radius
+              ),
+              child: Text(
+                'This is some text inside a box.',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
           ],
         ),
       ),
       //bottomNavigationBar: CustomNavigationBar(),
     );
-
   }
 }
