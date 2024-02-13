bfeb7ee95eacc01d36cd6165ba5d42d9a6792ebc
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-13 11:06:25 +0530

    modify: import buttin in dary

--- myapp/lib/diary.dart
+++ myapp/lib/diary.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:app/myButton.dart';
 
 class Diary extends StatelessWidget {
   const Diary({super.key});
