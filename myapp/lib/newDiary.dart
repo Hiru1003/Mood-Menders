fde33545c1c99c75c3289188c42b200732440bfd
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-14 21:45:12 +0530

    modify: new diary

--- myapp/lib/newDiary.dart
+++ myapp/lib/newDiary.dart
@@ -1,10 +1,27 @@
-import 'package:flutter/material.dart   ';
+import 'package:flutter/material.dart';
 
 class NewDiary extends StatelessWidget {
-  const NewDiary({super.key});
+  const NewDiary({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Diary'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(10.0), // Add some margin
+        padding: EdgeInsets.all(10.0), // Add some padding
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Text(
+          'Your text here',
+          style: TextStyle(fontSize: 16.0),
+        ),
+      ),
+    );
   }
 }
