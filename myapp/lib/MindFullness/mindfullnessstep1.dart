8049f8132ac2d8fb683ae31e9cd018ba4b4a27d9
Hiru1003 <hirumiperera10@gmail.com>
2024-02-14 07:47:32 +0530

    style: step 1

---
+++ myapp/lib/MindFullness/mindfullnessstep1.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class mindfulnessstep1_page extends StatefulWidget {
+  const mindfulnessstep1_page({
+    super.key,
+  });
+
+  @override
+  State<mindfulnessstep1_page> createState() => _mindfulnessstep1_pageState();
+}
+
+class _mindfulnessstep1_pageState extends State<mindfulnessstep1_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
