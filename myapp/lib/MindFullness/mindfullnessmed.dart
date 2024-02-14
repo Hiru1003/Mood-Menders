cbafcdcfbf1c6b2c8420d428849257bf876eaf63
Hiru1003 <hirumiperera10@gmail.com>
2024-02-14 08:10:48 +0530

    style: remove unwanted files

--- myapp/lib/MindFullness/mindfullnessmed.dart
+++ myapp/lib/MindFullness/mindfullnessmed.dart
@@ -1,6 +1,5 @@
 import 'package:app/MindFullness/mindfullnessstep1.dart';
 import 'package:app/button.dart';
-import 'package:app/meditaionmethods.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
