5eea54841631486d70c4c43af06a960a9144ca1c
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-15 00:12:49 +0530

    modify: profile

--- myapp/lib/profile.dart
+++ myapp/lib/profile.dart
@@ -5,9 +5,9 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: const Text('Profile'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Profile Page'),
       ),
     );
