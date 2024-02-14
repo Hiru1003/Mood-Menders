f37d78cc42cc1a9a82cfdefa592716d2b94c5ee7
Hiru1003 <hirumiperera10@gmail.com>
2024-02-14 07:51:16 +0530

    style: image for step one

--- myapp/lib/MindFullness/mindfullnessstep1.dart
+++ myapp/lib/MindFullness/mindfullnessstep1.dart
@@ -14,45 +14,67 @@ class _mindfulnessstep1_pageState extends State<mindfulnessstep1_page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(
-            left: 10,
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'MINDFULLNESS \nMEDITATION',
+                  style: GoogleFonts.poppins(
+                      textStyle: Theme.of(context).textTheme.displayLarge,
+                      color: const Color.fromARGB(255, 70, 66, 68),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400),
+                ),
+                Image.asset(
+                  'lib/images/leaf.png',
+                  height: 72,
+                )
+              ],
+            ),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'MINDFULLNESS \nMEDITATION',
-                style: GoogleFonts.poppins(
-                    textStyle: Theme.of(context).textTheme.displayLarge,
-                    color: const Color.fromARGB(255, 70, 66, 68),
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400),
+          titleSpacing: 4.0,
+          toolbarHeight: 65,
+          toolbarOpacity: 0.9,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(18),
+                bottomLeft: Radius.circular(18)),
+          ),
+          iconTheme: const IconThemeData(
+            color: Colors.black, //change your color here
+          ),
+          elevation: 0.00,
+          backgroundColor: Color.fromARGB(255, 134, 208, 203),
+        ),
+        backgroundColor: const Color.fromARGB(251, 241, 255, 252),
+        body: SingleChildScrollView(
+            child: Column(children: [
+          //image
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Container(
+              padding: const EdgeInsets.all(1.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //image
+                  Image.asset(
+                    'lib/images/step1.png',
+                    height: 240,
+                  )
+                ],
               ),
-              Image.asset(
-                'lib/images/leaf.png',
-                height: 72,
-              )
-            ],
+            ),
           ),
-        ),
-        titleSpacing: 4.0,
-        toolbarHeight: 65,
-        toolbarOpacity: 0.9,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(18),
-              bottomLeft: Radius.circular(18)),
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.black, //change your color here
-        ),
-        elevation: 0.00,
-        backgroundColor: Color.fromARGB(255, 134, 208, 203),
-      ),
-      backgroundColor: const Color.fromARGB(251, 241, 255, 252),
-    );
+        ])));
   }
 }
