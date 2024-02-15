cb95efbd5bd77cff1781d132f834e4c701581eae
Hiru1003 <hirumiperera10@gmail.com>
2024-02-15 20:19:33 +0530

    style: add new logo

--- myapp/lib/breathawarenessstep4.dart
+++ myapp/lib/breathawarenessstep4.dart
@@ -33,7 +33,7 @@ class _breathawarenessstep4_pageState extends State<breathawarenessstep4_page> {
                       fontWeight: FontWeight.w400),
                 ),
                 Image.asset(
-                  'lib/images/bg.png',
+                  'lib/images/leaf.png',
                   height: 72,
                 )
               ],
