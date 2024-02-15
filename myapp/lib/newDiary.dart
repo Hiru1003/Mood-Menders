caf7dac0d4f3ab498531fe026f26455746019325
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-15 20:39:59 +0530

    style: pic 2

--- myapp/lib/newDiary.dart
+++ myapp/lib/newDiary.dart
@@ -87,7 +87,7 @@ class NewDiary extends StatelessWidget {
     Expanded(
       child: Padding(
         padding: EdgeInsets.all(8.0),
-        child: Image.asset('lib/images/loveearth (1).png', fit: BoxFit.scaleDown),
+        child: Image.asset('lib/images/loveearth (2).png', fit: BoxFit.scaleDown),
       ),
     ),
     Expanded(
