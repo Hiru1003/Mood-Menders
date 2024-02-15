04e9f163da8e9514d363628c4b759c628293304b
Hiru1003 <hirumiperera10@gmail.com>
2024-02-15 09:00:45 +0530

    style: add text

--- myapp/lib/breathawarenessmed.dart
+++ myapp/lib/breathawarenessmed.dart
@@ -15,44 +15,78 @@ class _breathawarenessmed_pageState extends State<breathawarenessmed_page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(
-            left: 10,
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'BREATH AWARENESS \nMEDITATION',
+                  style: GoogleFonts.poppins(
+                      textStyle: Theme.of(context).textTheme.displayLarge,
+                      color: const Color.fromARGB(255, 70, 66, 68),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400),
+                ),
+                Image.asset(
+                  'lib/images/leaf.png',
+                  height: 72,
+                )
+              ],
+            ),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'MINDFULLNESS \nMEDITATION',
-                style: GoogleFonts.poppins(
-                    textStyle: Theme.of(context).textTheme.displayLarge,
-                    color: const Color.fromARGB(255, 70, 66, 68),
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400),
+          titleSpacing: 4.0,
+          toolbarHeight: 65,
+          toolbarOpacity: 0.9,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(18),
+                bottomLeft: Radius.circular(18)),
+          ),
+          iconTheme: const IconThemeData(
+            color: Colors.black, //change your color here
+          ),
+          elevation: 0.00,
+          backgroundColor: Color.fromARGB(255, 134, 208, 203),
+        ),
+        backgroundColor: const Color.fromARGB(251, 241, 255, 252),
+        body: SingleChildScrollView(
+            child: Column(children: [
+          //description
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 204, 248, 245),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16))),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //message
+                    Text(
+                      'Breif Description',
+                      style: GoogleFonts.poppins(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          color: const Color.fromARGB(255, 70, 66, 68),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
               ),
-              Image.asset(
-                'lib/images/leaf.png',
-                height: 72,
-              )
-            ],
+            ),
           ),
-        ),
-        titleSpacing: 4.0,
-        toolbarHeight: 65,
-        toolbarOpacity: 0.9,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(18),
-              bottomLeft: Radius.circular(18)),
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.black, //change your color here
-        ),
-        elevation: 0.00,
-        backgroundColor: Color.fromARGB(255, 134, 208, 203),
-      ),
-    );
+        ])));
   }
 }
