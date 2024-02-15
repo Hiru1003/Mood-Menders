d91c93a7e4b032663866cc0f7fafa7c40e3c3eea
Hiru1003 <hirumiperera10@gmail.com>
2024-02-15 08:54:33 +0530

    style: breathawarenessmed page

---
+++ myapp/lib/breathawarenessmed.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class breathawarenessmed_page extends StatefulWidget {
+  const breathawarenessmed_page({
+    super.key,
+  });
+
+  @override
+  State<breathawarenessmed_page> createState() =>
+      _breathawarenessmed_pageState();
+}
+
+class _breathawarenessmed_pageState extends State<breathawarenessmed_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
