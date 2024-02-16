5b0260c7fdc209f069fda6fc435c96633b7f39ba
Hiru1003 <hirumiperera10@gmail.com>
2024-02-16 08:23:39 +0530

    feat: column

--- myapp/lib/mindfulnessmed.dart
+++ myapp/lib/mindfulnessmed.dart
@@ -53,6 +53,6 @@ class _mindfulnessmed_pageState extends State<mindfulnessmed_page> {
           backgroundColor: Color.fromARGB(255, 134, 208, 203),
         ),
         backgroundColor: const Color.fromARGB(251, 241, 255, 252),
-        body: SingleChildScrollView());
+        body: SingleChildScrollView(child: Column(children: [])));
   }
 }
