250419298951d2da8fb3c04e3ccc3be927156d04
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-16 21:26:36 +0530

    fix: widget

--- myapp/lib/newDiary2.dart
+++ myapp/lib/newDiary2.dart
@@ -1,6 +1,7 @@
 import 'package:app/appBarD.dart';
 import 'package:app/gradientButton.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NewDiary2 extends StatelessWidget {
   const NewDiary2({Key? key}) : super(key: key);
