0b57e485db351a442daa4a5f0be81617132e14be
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-16 12:51:54 +0530

    modify: newDIary

--- myapp/lib/newDiary.dart
+++ myapp/lib/newDiary.dart
@@ -9,50 +9,7 @@ class NewDiary extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(
-            left: 10,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 5.0, top:20.0), 
-                    child: Text(
-                      'SECRET DIARY',
-                      style: GoogleFonts.poppins(
-                        textStyle: Theme.of(context).textTheme.headline4,
-                        color: const Color.fromARGB(255, 70, 66, 68),
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Image.asset(
-                'lib/images/diary1.png',
-                fit: BoxFit.cover,
-              ),
-            ],
-          ),
-        ),
-        titleSpacing: 4.0,
-        toolbarHeight: 130,
-        toolbarOpacity: 0.9,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(18),
-              bottomLeft: Radius.circular(18)),
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.black,
-        ),
-        elevation: 0.00,
-        backgroundColor: const Color.fromARGB(255, 134, 208, 203),
+        CustomAppBar(),
       ),
     body: Container(
       width: double.infinity,
