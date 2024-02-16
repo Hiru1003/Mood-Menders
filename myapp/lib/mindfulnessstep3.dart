428b8a6c9e52cf7faecd29b94b1fb6ec4c9d649d
Hiru1003 <hirumiperera10@gmail.com>
2024-02-16 13:21:59 +0530

    stylr: step 3

---
+++ myapp/lib/mindfulnessstep3.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class mindfulnessstep3_page extends StatefulWidget {
+  const mindfulnessstep3_page({
+    super.key,
+  });
+
+  @override
+  State<mindfulnessstep3_page> createState() => _mindfulnessstep3_pageState();
+}
+
+class _mindfulnessstep3_pageState extends State<mindfulnessstep3_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
