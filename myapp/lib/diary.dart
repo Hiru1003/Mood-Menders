6f915eeb6ac74dba34b7f6dd6f36e38c3f903eeb
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-16 12:56:41 +0530

    modify: diary appBar

--- myapp/lib/diary.dart
+++ myapp/lib/diary.dart
@@ -15,59 +15,7 @@ class Diary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 5.0, top: 20.0),
-                    child: Text(
-                      'SECRET DIARY',
-                      style: GoogleFonts.poppins(
-                        textStyle: Theme.of(context).textTheme.headlineMedium,
-                        color: const Color.fromARGB(255, 70, 66, 68),
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  // Padding(
-                  //   padding: const EdgeInsets.only(right: 36, top: 16),
-                  //   child: myButton.SButton(
-                  //     onTap: () {
-                  //       Get.to(newDiary.NewDiary());
-                  //     },
-                  //   ),
-                  // ),
-                ],
-                
-              ),
-              Image.asset(
-                'lib/images/diary1.png',
-                fit: BoxFit.cover,
-              ),
-            ],
-          ),
-        ),
-        titleSpacing: 4.0,
-        toolbarHeight: 130,
-        toolbarOpacity: 0.9,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(18),
-              bottomLeft: Radius.circular(18)),
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.black,
-        ),
-        elevation: 0.00,
-        backgroundColor: const Color.fromARGB(255, 134, 208, 203),
-      ),
+      appBar:
       backgroundColor: const Color.fromARGB(251, 241, 255, 252),
       body: SingleChildScrollView(
         child: Column(
