36238478b3561c62fb3aa803ffc4d4477235057e
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-16 12:49:50 +0530

    modify: appbar

---
+++ myapp/lib/appBarD.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      
+    );
+  }
+
+}
