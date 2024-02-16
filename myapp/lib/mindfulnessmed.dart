f02134f88e131860275d856b20963531967fa3c6
Hiru1003 <hirumiperera10@gmail.com>
2024-02-16 06:53:20 +0530

    modify: topic font weight mindfullness med

--- myapp/lib/mindfulnessmed.dart
+++ myapp/lib/mindfulnessmed.dart
@@ -29,7 +29,7 @@ class _mindfulnessmed_pageState extends State<mindfulnessmed_page> {
                     textStyle: Theme.of(context).textTheme.displayLarge,
                     color: const Color.fromARGB(255, 70, 66, 68),
                     fontSize: 18,
-                    fontWeight: FontWeight.w400),
+                    fontWeight: FontWeight.w500),
               ),
               Image.asset(
                 'lib/images/leaf.png',
