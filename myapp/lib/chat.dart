6e968506108963a55491318519eace1c9391f716
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-16 20:28:10 +0530

    modify: chat

--- myapp/lib/chat.dart
+++ myapp/lib/chat.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Chat extends StatelessWidget {
+  const Chat({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
