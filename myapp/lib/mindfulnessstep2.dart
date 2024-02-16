c1604c78c15a81bb32f24e42e133f0a95dd14ae1
Hiru1003 <hirumiperera10@gmail.com>
2024-02-16 13:04:30 +0530

    style: bg color

--- myapp/lib/mindfulnessstep2.dart
+++ myapp/lib/mindfulnessstep2.dart
@@ -13,6 +13,8 @@ class mindfulnessstep2_page extends StatefulWidget {
 class _mindfulnessstep2_pageState extends State<mindfulnessstep2_page> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(251, 241, 255, 252),
+    );
   }
 }
