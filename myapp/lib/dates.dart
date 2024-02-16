2a05598418d7cee334d1839ecfd465d9407d8494
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-16 20:24:29 +0530

    dates

--- myapp/lib/dates.dart
+++ myapp/lib/dates.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class DatesWidget extends StatelessWidget {
+  const DatesWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     DateTime today = DateTime.now();
