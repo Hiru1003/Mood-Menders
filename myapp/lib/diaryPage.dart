a0c121052deae216970c489aed16a58b0d458fe3
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-16 13:20:26 +0530

    modify: scrollable diaryPage

--- myapp/lib/diaryPage.dart
+++ myapp/lib/diaryPage.dart
@@ -18,68 +18,70 @@ class DiaryPage extends StatelessWidget {
           color: const Color.fromARGB(255, 204, 248, 245),
           borderRadius: BorderRadius.circular(10),
         ),
-        child: Column(
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Image.asset(
-                      'lib/images/loveearth (1).png',
-                      width: 60,
-                      height: 60,
-                    ),
-                    const SizedBox(width: 10),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          const Text(
-                            'Happy',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          Text(
-                            DateFormat('d MMMM yyyy, h:mm a').format(DateTime.now()),
-                            style: const TextStyle(fontSize: 13),
-                          ),
-                        ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Image.asset(
+                        'lib/images/loveearth (1).png',
+                        width: 60,
+                        height: 60,
                       ),
+                      const SizedBox(width: 10),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            const Text(
+                              'Happy',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              DateFormat('d MMMM yyyy, h:mm a').format(DateTime.now()),
+                              style: const TextStyle(fontSize: 13),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: Image.asset(
+                      'lib/images/love 1.png',
+                      width: 30,
+                      height: 30,
                     ),
-                  ],
-                ),
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: Image.asset(
-                    'lib/images/love 1.png',
-                    width: 30,
-                    height: 30,
+                  ),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'I got stickers from FOSS',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'I got stickers from FOSS',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et est leo.',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ],
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
