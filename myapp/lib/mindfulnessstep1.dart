e96e76f50f533c5f9c5b380019c98e1e9c1ccc07
Hiru1003 <hirumiperera10@gmail.com>
2024-02-16 12:58:37 +0530

    feat: navigation to step 2

--- myapp/lib/mindfulnessstep1.dart
+++ myapp/lib/mindfulnessstep1.dart
@@ -1,5 +1,5 @@
 import 'package:app/button.dart';
-import 'package:app/meditaionmethods.dart';
+import 'package:app/mindfulnessstep2.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
