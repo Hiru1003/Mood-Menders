0c17fd251613818b0bf53674f2082fef20556201
Hiru1003 <hirumiperera10@gmail.com>
2024-02-16 06:58:59 +0530

    feat: step 5 breath awareness

---
+++ myapp/lib/breathawarenessstep5.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class breathawarenessstep5_page extends StatefulWidget {
+  const breathawarenessstep5_page({
+    super.key,
+  });
+
+  @override
+  State<breathawarenessstep5_page> createState() =>
+      _breathawarenessstep5_pageState();
+}
+
+class _breathawarenessstep5_pageState extends State<breathawarenessstep5_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
