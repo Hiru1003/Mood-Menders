4e1d498388b58e02bdd573340b9072394960413d
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-16 13:01:34 +0530

    modify: diaryPAGE

--- myapp/lib/diaryPage.dart
+++ myapp/lib/diaryPage.dart
@@ -1,8 +1,237 @@
+import 'package:app/appBarD.dart';
+import 'package:app/gradientButton.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class CustomAppBar extends StatelessWidget {
+class NewDiary extends StatelessWidget {
+  const NewDiary({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return CustomAppBar();
+    return Scaffold(
+      appBar: 
+        CustomAppBar(),
+    body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 204, 248, 245),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 12, bottom: 8),
+            child: Text(
+              'What is your mood today?',
+              style: GoogleFonts.poppins(
+                textStyle: Theme.of(context).textTheme.headlineMedium,
+                color: const Color.fromARGB(255, 70, 66, 68),
+                fontSize: 24,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          Row(
+  children: <Widget>[
+    Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.asset('lib/images/loveearth (1).png', fit: BoxFit.scaleDown),
+      ),
+    ),
+    Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.asset('lib/images/loveearth (2).png', fit: BoxFit.scaleDown),
+      ),
+    ),
+    Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.asset('lib/images/loveearth (3).png', fit: BoxFit.scaleDown),
+      ),
+    ),
+    Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.asset('lib/images/loveearth (4).png', fit: BoxFit.scaleDown),
+      ),
+    ),
+    Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.asset('lib/images/loveearth (5).png', fit: BoxFit.scaleDown),
+      ),
+    ),
+  ],
+),
+Padding(
+            padding: const EdgeInsets.only(top: 12, bottom: 8),
+            child: Text(
+              'Emotions',
+              style: GoogleFonts.poppins(
+                textStyle: Theme.of(context).textTheme.headlineMedium,
+                color: const Color.fromARGB(255, 70, 66, 68),
+                fontSize: 24,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: <Widget>[
+    Padding(
+      padding: const EdgeInsets.all(3.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Excited'),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(3.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Proud'),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(3.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Lonely'),
+      ),
+    ),
+  ],
+),
+Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: <Widget>[
+    Padding(
+      padding: const EdgeInsets.all(3),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Stressed'),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(3),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Tired'),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(3),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Annoyed'),
+      ),
+    ),
+  ],
+),
+Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: <Widget>[
+    Padding(
+      padding: const EdgeInsets.all(3),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Happy'),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(3),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Angry'),
+      ),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(3),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Joyful'),
+      ),
+    ),
+  ],
+),
+Padding(
+            padding: const EdgeInsets.only(top: 12, bottom: 8),
+            child: Text(
+              'Emotions',
+              style: GoogleFonts.poppins(
+                textStyle: Theme.of(context).textTheme.headlineMedium,
+                color: const Color.fromARGB(255, 70, 66, 68),
+                fontSize: 24,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+           Row(
+  children: <Widget>[
+    Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(26.0),
+        child: Image.asset('lib/images/edu.png', fit: BoxFit.scaleDown),
+      ),
+    ),
+    Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(26.0),
+        child: Image.asset('lib/images/love.png', fit: BoxFit.scaleDown),
+      ),
+    ),
+    Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(26.0),
+        child: Image.asset('lib/images/famliy.png', fit: BoxFit.scaleDown),
+      ),
+    ),
+    Expanded( 
+      child: Padding(
+        padding: const EdgeInsets.all(26.0),
+        child: Image.asset('lib/images/health.png', fit: BoxFit.scaleDown),
+      ),
+    ),
+  ],
+),
+Row(
+  children: <Widget>[
+    Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(26.0),
+        child: Image.asset('lib/images/personal.png', fit: BoxFit.scaleDown),
+      ),
+    ),
+    Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(26.0),
+        child: Image.asset('lib/images/finance.png', fit: BoxFit.scaleDown),
+      ),
+    ),
+    Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(26.0),
+        child: Image.asset('lib/images/friends.png', fit: BoxFit.scaleDown),
+      ),
+    ),
+    Expanded( 
+      child: Padding(
+        padding: const EdgeInsets.all(26.0),
+        child: Image.asset('lib/images/leisure.png', fit: BoxFit.scaleDown),
+      ),
+    ),
+  ],
+),
+GradientButton(),
+        ],
+      ),
+    ),
+    )
+    );
   }
 }
