749a77f91d66085fb25ca5863bb24baed1e0928b
Hiru1003 <hirumiperera10@gmail.com>
2024-02-16 12:58:04 +0530

    style: scaffold step 2

---
+++ myapp/lib/mindfulnessstep2.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class mindfulnessstep2_page extends StatefulWidget {
+  const mindfulnessstep2_page({
+    super.key,
+  });
+
+  @override
+  State<mindfulnessstep2_page> createState() => _mindfulnessstep2_pageState();
+}
+
+class _mindfulnessstep2_pageState extends State<mindfulnessstep2_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
