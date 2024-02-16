d100ec10290fc40da4e33cb63a8514a25c2a0906
Hiru1003 <hirumiperera10@gmail.com>
2024-02-16 13:08:16 +0530

    style:column

--- myapp/lib/mindfulnessstep2.dart
+++ myapp/lib/mindfulnessstep2.dart
@@ -53,6 +53,6 @@ class _mindfulnessstep2_pageState extends State<mindfulnessstep2_page> {
           backgroundColor: Color.fromARGB(255, 134, 208, 203),
         ),
         backgroundColor: const Color.fromARGB(251, 241, 255, 252),
-        body: SingleChildScrollView());
+        body: SingleChildScrollView(child: Column(children: [])));
   }
 }
