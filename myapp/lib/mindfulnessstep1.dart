71a405a7f88ba3b8d6967d6627befd1a131576dd
Hiru1003 <hirumiperera10@gmail.com>
2024-02-16 12:50:41 +0530

    style: logo change

--- myapp/lib/mindfulnessstep1.dart
+++ myapp/lib/mindfulnessstep1.dart
@@ -34,7 +34,7 @@ class _mindfulnessstep1_pageState extends State<mindfulnessstep1_page> {
                     fontWeight: FontWeight.w400),
               ),
               Image.asset(
-                'lib/images/bg.png',
+                'lib/images/leaf.png',
                 height: 72,
               )
             ],
