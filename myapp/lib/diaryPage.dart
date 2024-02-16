19ea007cc9e490e5535803ba0efd5363cd6b11ea
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-16 13:12:03 +0530

    modify: diaryPage

--- myapp/lib/diaryPage.dart
+++ myapp/lib/diaryPage.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
-class CustomAppBar extends StatelessWidget {
+class DiaryPage extends StatelessWidget {
+  const DiaryPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return CustomAppBar();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Diary Page'),
+      ),
+      body: Center(
+        child: Text(
+          'This is the Diary Page',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
   }
 }
