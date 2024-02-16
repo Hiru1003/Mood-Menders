43ba698dfec756166826753c618bbb28735cf1d3
Hiru1003 <hirumiperera10@gmail.com>
2024-02-16 13:19:27 +0530

    modify: text

--- myapp/lib/mindfulnessstep2.dart
+++ myapp/lib/mindfulnessstep2.dart
@@ -121,6 +121,8 @@ class _mindfulnessstep2_pageState extends State<mindfulnessstep2_page> {
                           textStyle: Theme.of(context).textTheme.displayLarge,
                           color: const Color.fromARGB(255, 70, 66, 68),
                           fontSize: 13,
+                          height: 1.2,
+                          wordSpacing: 1.8,
                           fontWeight: FontWeight.w400),
                     ),
                   ],
