6fc6ceab0f8a0196f505e90c7596d6a169de4962
Hiru1003 <hirumiperera10@gmail.com>
2024-02-16 07:18:48 +0530

    modify:word spacing

--- myapp/lib/breathawarenessstep6.dart
+++ myapp/lib/breathawarenessstep6.dart
@@ -123,6 +123,8 @@ class _breathawarenessstep6_pageState extends State<breathawarenessstep6_page> {
                           textStyle: Theme.of(context).textTheme.displayLarge,
                           color: const Color.fromARGB(255, 70, 66, 68),
                           fontSize: 13,
+                          height: 1.2,
+                          wordSpacing: 1.8,
                           fontWeight: FontWeight.w400),
                     ),
                   ],
