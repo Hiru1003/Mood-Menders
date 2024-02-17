6a4fb17d26d75c34bc495752c74eae1cd7c2912c
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-17 20:27:55 +0530

    fix: errors

--- myapp/lib/newDiary.dart
+++ myapp/lib/newDiary.dart
@@ -2,6 +2,7 @@ import 'package:app/appBarD.dart';
 import 'package:app/diary.dart';
 import 'package:app/gradientButton.dart';
 import 'package:app/newDiary2.dart';
+import 'package:app/recordevent.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
