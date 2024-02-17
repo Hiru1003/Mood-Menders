33f815d93761a4df004fead726a8d9837feb490a
Hiru1003 <hirumiperera10@gmail.com>
2024-02-17 14:10:49 +0530

    style: container

--- myapp/lib/mindfulnessstep5.dart
+++ myapp/lib/mindfulnessstep5.dart
@@ -105,7 +105,15 @@ class _mindfulnessstep5_pageState extends State<mindfulnessstep5_page> {
           //para
           Padding(
             padding: const EdgeInsets.only(left: 10, right: 10),
-            child: Container(),
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [],
+                ),
+              ),
+            ),
           ),
         ])));
   }
