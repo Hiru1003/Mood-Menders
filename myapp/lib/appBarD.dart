29c2be31b8079aa3de53f4b2c9a6dd7e1c0afabe
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-17 13:24:11 +0530

    modify: remove button

--- myapp/lib/appBarD.dart
+++ myapp/lib/appBarD.dart
@@ -1,5 +1,3 @@
-import 'package:app/myButton.dart';
-import 'package:app/newDiary.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -29,17 +27,6 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
                       fontSize: 24,
                       fontWeight: FontWeight.w600,
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 40.0, top: 20.0),
-                  child: SButton(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => NewDiary()),
-                      );
-                    },
                   ),
                 ),
               ],
