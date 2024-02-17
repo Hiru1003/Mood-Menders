23790c52503527107908dc239a4fc471f4fdec6b
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-17 22:35:27 +0530

    style: container

--- myapp/lib/stat.dart
+++ myapp/lib/stat.dart
@@ -56,7 +56,7 @@ class Stat extends StatelessWidget {
       backgroundColor: const Color.fromARGB(251, 241, 255, 252),
       body: Container(
             width: double.infinity,
-            height: double.infinity,
+            height: 250,
             padding: const EdgeInsets.all(15),
             margin: const EdgeInsets.all(15),
             decoration: BoxDecoration(
