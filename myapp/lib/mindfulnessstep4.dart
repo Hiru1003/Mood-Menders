1f100b39e85b4fda1b6fe3fba5843df5c7880b3f
Hiru1003 <hirumiperera10@gmail.com>
2024-02-17 12:34:19 +0530

    style: word spacing

--- myapp/lib/mindfulnessstep4.dart
+++ myapp/lib/mindfulnessstep4.dart
@@ -120,6 +120,8 @@ class _mindfulnessstep4_pageState extends State<mindfulnessstep4_page> {
                           textStyle: Theme.of(context).textTheme.displayLarge,
                           color: const Color.fromARGB(255, 70, 66, 68),
                           fontSize: 13,
+                          height: 1.2,
+                          wordSpacing: 1.8,
                           fontWeight: FontWeight.w400),
                     ),
                   ],
