2a6eaad39616ecf449230f7842f67fa18d2e2b38
Hiru1003 <hirumiperera10@gmail.com>
2024-02-17 14:02:53 +0530

    style: text

--- myapp/lib/mindfulnessstep5.dart
+++ myapp/lib/mindfulnessstep5.dart
@@ -15,13 +15,30 @@ class _mindfulnessstep5_pageState extends State<mindfulnessstep5_page> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      title: Padding(
-        padding: const EdgeInsets.only(left: 10,),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-        ]))
+        title: Padding(
+          padding: const EdgeInsets.only(
+            left: 10,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'MINDFULLNESS \nMEDITATION',
+                style: GoogleFonts.poppins(
+                    textStyle: Theme.of(context).textTheme.displayLarge,
+                    color: const Color.fromARGB(255, 70, 66, 68),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400),
+              ),
+              Image.asset(
+                'lib/images/bg.png',
+                height: 72,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
