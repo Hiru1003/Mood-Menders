ae33f9e91597487c027cd61f720be09fcdc4a1ae
Hiru1003 <hirumiperera10@gmail.com>
2024-02-17 17:58:13 +0530

    feat:button

--- myapp/lib/mindfulnessstep6.dart
+++ myapp/lib/mindfulnessstep6.dart
@@ -126,6 +126,25 @@ class _mindfulnessstep6_pageState extends State<mindfulnessstep6_page> {
               ),
             ),
           ),
+          const SizedBox(
+            height: 25,
+          ),
+          //continue button
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: MyButton(
+              buttontext: 'Next Step',
+              onTap: () {
+                {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => (const mindfulnessstep7_page())),
+                  );
+                }
+              },
+            ),
+          ),
         ])));
   }
 }
