422d17eb4f9da3386c458dbe9c779cdb5b2b3495
Hiru1003 <hirumiperera10@gmail.com>
2024-02-17 12:21:11 +0530

    style: app bar container

--- myapp/lib/mindfulnessstep3.dart
+++ myapp/lib/mindfulnessstep3.dart
@@ -13,6 +13,37 @@ class mindfulnessstep3_page extends StatefulWidget {
 class _mindfulnessstep3_pageState extends State<mindfulnessstep3_page> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(
+            left: 10,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'MINDFULLNESS \nMEDITATION',
+                style: GoogleFonts.poppins(
+                    textStyle: Theme.of(context).textTheme.displayLarge,
+                    color: const Color.fromARGB(255, 70, 66, 68),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400),
+              ),
+              Image.asset(
+                'lib/images/bg.png',
+                height: 72,
+              )
+            ],
+          ),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.black, //change your color here
+        ),
+        elevation: 0.00,
+        backgroundColor: Color.fromARGB(255, 134, 208, 203),
+      ),
+    );
   }
 }
