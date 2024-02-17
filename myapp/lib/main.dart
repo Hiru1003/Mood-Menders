e5b9db272a773990fafe94cbf9b6914048632126
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-17 12:58:55 +0530

    modify: main

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app/newDiary2.dart';
+import 'package:app/diary.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false, 
-        //home: loading_page()
-        home: NewDiary2(),
-        );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, 
+      home: Diary(), 
+    );
   }
 }
