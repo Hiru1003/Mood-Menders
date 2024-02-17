62ae59246e20da5a512ecfda67e1485dd65868e1
Hiru1003 <hirumiperera10@gmail.com>
2024-02-17 14:08:52 +0530

    style:SizedBox

--- myapp/lib/mindfulnessstep5.dart
+++ myapp/lib/mindfulnessstep5.dart
@@ -74,6 +74,9 @@ class _mindfulnessstep5_pageState extends State<mindfulnessstep5_page> {
               ),
             ),
           ),
+          const SizedBox(
+            height: 20,
+          ),
         ])));
   }
 }
