97716246906761852e360fccb02bb712033db699
Hiru1003 <hirumiperera10@gmail.com>
2024-02-17 14:02:16 +0530

    style: maximum alignment

--- myapp/lib/mindfulnessstep5.dart
+++ myapp/lib/mindfulnessstep5.dart
@@ -13,6 +13,15 @@ class mindfulnessstep5_page extends StatefulWidget {
 class _mindfulnessstep5_pageState extends State<mindfulnessstep5_page> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+      title: Padding(
+        padding: const EdgeInsets.only(left: 10,),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+        ]))
+    );
   }
 }
