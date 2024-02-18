e069000e14876e051d907ab3ed11e30425be5967
Hiru1003 <hirumiperera10@gmail.com>
2024-02-18 12:59:29 +0530

    style:SizedBox

--- myapp/lib/mindfulnessstep7.dart
+++ myapp/lib/mindfulnessstep7.dart
@@ -53,6 +53,11 @@ class _mindfulnessstep7_pageState extends State<mindfulnessstep7_page> {
           backgroundColor: Color.fromARGB(255, 134, 208, 203),
         ),
         backgroundColor: const Color.fromARGB(251, 241, 255, 252),
-        body: SingleChildScrollView(child: Column(children: [])));
+        body: SingleChildScrollView(
+            child: Column(children: [
+          const SizedBox(
+            height: 20,
+          ),
+        ])));
   }
 }
