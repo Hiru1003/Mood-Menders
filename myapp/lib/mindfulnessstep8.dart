4c20e3bea7c5bf68c15b1b6c636dd6305ac8ae69
Hiru1003 <hirumiperera10@gmail.com>
2024-02-18 19:15:47 +0530

    style:fontweight

--- myapp/lib/mindfulnessstep8.dart
+++ myapp/lib/mindfulnessstep8.dart
@@ -31,7 +31,7 @@ class _mindfulnessstep8_pageState extends State<mindfulnessstep8_page> {
                       textStyle: Theme.of(context).textTheme.displayLarge,
                       color: const Color.fromARGB(255, 70, 66, 68),
                       fontSize: 18,
-                      fontWeight: FontWeight.w400),
+                      fontWeight: FontWeight.w500),
                 ),
                 Image.asset(
                   'lib/images/leaf.png',
