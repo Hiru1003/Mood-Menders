b98644be54005d3e051060fadc8a2e5109c71c89
Hiru1003 <hirumiperera10@gmail.com>
2024-02-18 13:35:22 +0530

    style: text

--- myapp/lib/mindfulnessstep8.dart
+++ myapp/lib/mindfulnessstep8.dart
@@ -21,7 +21,14 @@ class _mindfulnessstep8_pageState extends State<mindfulnessstep8_page> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+        Text('MINDFULLNESS \nMEDITATION',style: GoogleFonts.poppins(
+          textStyle: Theme.of(context).textTheme.displayLarge,
+          color: const Color.fromARGB(255, 70, 66, 68),
+          fontSize: 18,
+          fontWeight: FontWeight.w400
+          ), ),
         
+      ),
 
     );
   }
