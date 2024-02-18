01a56e48cc276aa160ffeddf905cb595004031f4
Hiru1003 <hirumiperera10@gmail.com>
2024-02-18 12:58:20 +0530

    style: bg color

--- myapp/lib/mindfulnessstep7.dart
+++ myapp/lib/mindfulnessstep7.dart
@@ -52,6 +52,7 @@ class _mindfulnessstep7_pageState extends State<mindfulnessstep7_page> {
         elevation: 0.00,
         backgroundColor: Color.fromARGB(255, 134, 208, 203),
       ),
+      backgroundColor: const Color.fromARGB(251, 241, 255, 252),
     );
   }
 }
