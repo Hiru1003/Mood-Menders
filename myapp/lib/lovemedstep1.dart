4063c259250709c8c7d72431937c528b2caad4c1
Hiru1003 <hirumiperera10@gmail.com>
2024-02-18 19:45:17 +0530

    style:row

--- myapp/lib/lovemedstep1.dart
+++ myapp/lib/lovemedstep1.dart
@@ -108,6 +108,15 @@ class _lovemedstep1_pageState extends State<lovemedstep1_page> {
             //para
             Padding(
               padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(1.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [],
+                  ),
+                ),
+              ),
             ),
           ],
         )));
