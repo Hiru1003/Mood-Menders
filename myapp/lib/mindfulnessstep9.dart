7ed4120c184e86ee615e05b2f36b73d4c4983a08
Hiru1003 <hirumiperera10@gmail.com>
2024-02-18 19:13:28 +0530

    style:opacity

--- myapp/lib/mindfulnessstep9.dart
+++ myapp/lib/mindfulnessstep9.dart
@@ -31,7 +31,14 @@ class _mindfulnessstep9_pageState extends State<mindfulnessstep9_page> {
           height: 72,)
           ],
           ),
-     
+      ),
+        titleSpacing: 4.0,
+        toolbarHeight: 65,
+        toolbarOpacity: 0.9,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(18),
+           bottomLeft: Radius.circular(18)),
       ),
     );
   }
