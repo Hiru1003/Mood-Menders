d13e435049f3568678aa3713c498f51dca7539d4
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-18 14:34:17 +0530

    modify: poll class

--- myapp/lib/poll.dart
+++ myapp/lib/poll.dart
@@ -1,7 +1,21 @@
 class Poll {
   String question;
   List<String> options;
+  List<int> votes;
   int selectedOptionIndex;
 
-  Poll({required this.question, required this.options, this.selectedOptionIndex = -1});
+  Poll({required this.question, required this.options, this.selectedOptionIndex = -1})
+      : votes = List.filled(options.length, 0);
+
+  void vote(int optionIndex) {
+    if (optionIndex >= 0 && optionIndex < options.length) {
+      votes[optionIndex]++;
+    }
+  }
+
+  double percentage(int optionIndex) {
+    int totalVotes = votes.reduce((a, b) => a + b);
+    return totalVotes == 0 ? 0 : (votes[optionIndex] / totalVotes) * 100;
+  }
 }
+
