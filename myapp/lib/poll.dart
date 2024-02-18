35343a540f32154032dcef5b4c30e816f02e485e
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-18 14:34:46 +0530

    modify: void main

--- myapp/lib/poll.dart
+++ myapp/lib/poll.dart
@@ -19,3 +19,19 @@ class Poll {
   }
 }
 
+void main() {
+  Poll poll = Poll(question: 'Question?', options: ['Option 1', 'Option 2', 'Option 3']);
+
+  // Simulate votes
+  for (int i = 0; i < 100; i++) {
+    if (i < 40) {
+      poll.vote(0); // 40 votes for Option 1
+    } else if (i < 70) {
+      poll.vote(1); // 30 votes for Option 2
+    } else {
+      poll.vote(2); // 30 votes for Option 3
+    }
+  }
+
+  print('Percentage for Option 1: ${poll.percentage(0)}%');
+}
