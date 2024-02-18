7e71d35e32585c2f4f90d2a6a18c8b2d20ea6123
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-18 14:38:25 +0530

    feat: percentage

---
+++ myapp/lib/percentageBar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class PercentageBar extends StatelessWidget {
+  final double percentage;
+  final Color barColor;
+  final Color backgroundColor;
+
+  const PercentageBar({
+    Key? key,
+    required this.percentage,
+    this.barColor = Colors.blue,
+    this.backgroundColor = Colors.grey,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 20,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: FractionallySizedBox(
+        widthFactor: percentage,
+        child: Container(
+          decoration: BoxDecoration(
+            color: barColor,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
+}
