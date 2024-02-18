7f2b49427d4f28bebcb68f25232ecdb98cf7763c
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-18 13:30:54 +0530

    fix: text

--- myapp/lib/stat.dart
+++ myapp/lib/stat.dart
@@ -145,32 +145,33 @@ class _StatState extends State<Stat> {
     seriesList as List<charts.Series<dynamic, num>>,
     defaultRenderer: new charts.LineRendererConfig(includeArea: true, stacked: true),
     primaryMeasureAxis: new charts.NumericAxisSpec(
-      renderSpec: new charts.NoneRenderSpec(), // this will hide Y axis values
+      renderSpec: new charts.NoneRenderSpec(),
     ),
     domainAxis: new charts.NumericAxisSpec(
-      tickProviderSpec: new charts.StaticNumericTickProviderSpec(
-        [
-          new charts.TickSpec(0, label: 'S'),
-          new charts.TickSpec(1, label: 'M'),
-          new charts.TickSpec(2, label: 'T'),
-          new charts.TickSpec(3, label: 'W'),
-          new charts.TickSpec(4, label: 'T'),
-          new charts.TickSpec(5, label: 'F'),
-          new charts.TickSpec(6, label: 'S'),
-        ],
-      ),
-      renderSpec: charts.GridlineRendererSpec(
-        labelStyle: charts.TextStyleSpec(
-          fontSize: 0, // size of text
-          color: charts.MaterialPalette.transparent, // color of text
+  tickProviderSpec: new charts.StaticNumericTickProviderSpec(
+    List.generate(7, (index) {
+      return new charts.TickSpec(
+        index,
+        label: ['S', 'M', 'T', 'W', 'T', 'F', 'S'][index],
+        style: new charts.TextStyleSpec(
+          fontSize: 13, 
+          color: charts.MaterialPalette.black,
         ),
-        lineStyle: charts.LineStyleSpec(
-          color: charts.MaterialPalette.transparent, // color of line
-        ),
-      ),
+      );
+    }),
+  ),
+  renderSpec: charts.GridlineRendererSpec(
+    labelStyle: charts.TextStyleSpec(
+      fontSize: 0,
+      color: charts.MaterialPalette.transparent, 
+    ),
+    lineStyle: charts.LineStyleSpec(
+      color: charts.MaterialPalette.transparent, 
     ),
   ),
 ),
+    ),
+  ),
               ],
             ),
       ),
