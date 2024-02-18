5dc0a4c3c0b82b3b645bc21e1d00c26bce56ca6a
Hiru1003 <hirumiperera10@gmail.com>
2024-02-18 19:32:03 +0530

    feat: love kindness meditaion step 1

---
+++ myapp/lib/lovemedstep1.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class lovemedstep1_page extends StatefulWidget {
+  const lovemedstep1_page({
+    super.key,
+  });
+
+  @override
+  State<lovemedstep1_page> createState() => _lovemedstep1_pageState();
+}
+
+class _lovemedstep1_pageState extends State<lovemedstep1_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
