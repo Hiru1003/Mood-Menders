e87b9f7e6667bab400c8369b926cbd5deb7de0f7
Hiru1003 <hirumiperera10@gmail.com>
2024-02-18 19:13:53 +0530

    style:icontheme

--- myapp/lib/mindfulnessstep9.dart
+++ myapp/lib/mindfulnessstep9.dart
@@ -15,30 +15,40 @@ class _mindfulnessstep9_pageState extends State<mindfulnessstep9_page> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      title: Padding(
-        padding: const EdgeInsets.only(left: 10,),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-        Text('MINDFULLNESS \nMEDITATION',style: GoogleFonts.poppins(
-          textStyle: Theme.of(context).textTheme.displayLarge,
-          color: const Color.fromARGB(255, 70, 66, 68),
-          fontSize: 18,
-          fontWeight: FontWeight.w400
-          ), ),
-          Image.asset('lib/images/bg.png',
-          height: 72,)
-          ],
+        title: Padding(
+          padding: const EdgeInsets.only(
+            left: 10,
           ),
-      ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'MINDFULLNESS \nMEDITATION',
+                style: GoogleFonts.poppins(
+                    textStyle: Theme.of(context).textTheme.displayLarge,
+                    color: const Color.fromARGB(255, 70, 66, 68),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400),
+              ),
+              Image.asset(
+                'lib/images/bg.png',
+                height: 72,
+              )
+            ],
+          ),
+        ),
         titleSpacing: 4.0,
         toolbarHeight: 65,
         toolbarOpacity: 0.9,
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.only(
-          bottomRight: Radius.circular(18),
-           bottomLeft: Radius.circular(18)),
+              bottomRight: Radius.circular(18),
+              bottomLeft: Radius.circular(18)),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.black, //change your color here
+        ),
       ),
     );
   }
