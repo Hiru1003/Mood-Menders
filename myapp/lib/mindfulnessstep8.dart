e3ceb74538538802334a8d0f9a44c340eb54e73d
Hiru1003 <hirumiperera10@gmail.com>
2024-02-18 13:34:13 +0530

    style: scaffold

---
+++ myapp/lib/mindfulnessstep8.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class mindfulnessstep8_page extends StatefulWidget {
+  const mindfulnessstep8_page({
+    super.key,
+  });
+
+  @override
+  State<mindfulnessstep8_page> createState() => _mindfulnessstep8_pageState();
+}
+
+class _mindfulnessstep8_pageState extends State<mindfulnessstep8_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
