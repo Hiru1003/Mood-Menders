a1c2b86684244b3228966497fe4dcddcd1b4fe3a
Hiru1003 <hirumiperera10@gmail.com>
2024-02-18 19:33:11 +0530

    style: appbar padding

--- myapp/lib/lovemedstep1.dart
+++ myapp/lib/lovemedstep1.dart
@@ -13,6 +13,16 @@ class lovemedstep1_page extends StatefulWidget {
 class _lovemedstep1_pageState extends State<lovemedstep1_page> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+      title: Padding(
+        padding: const EdgeInsets.only(left: 10,),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+      
+      ),
+    );
   }
 }
