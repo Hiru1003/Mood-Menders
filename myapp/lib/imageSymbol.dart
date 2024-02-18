a89de409997068391ddcbfc9d8b63ae4666813a2
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-18 21:27:52 +0530

    modify: image render

---
+++ myapp/lib/imageSymbol.dart
@@ -0,0 +1,21 @@
+class ImageSymbolRenderer extends charts.CircleSymbolRenderer {
+  final ui.Image image;
+
+  ImageSymbolRenderer(this.image);
+
+  @override
+  void paint(charts.ChartCanvas canvas, Rectangle<num> bounds,
+      {List<int>? dashPattern,
+      charts.Color? fillColor,
+      charts.FillPatternType? fillPattern,
+      charts.Color? strokeColor,
+      double? strokeWidthPx}) {
+    super.paint(canvas, bounds,
+        dashPattern: dashPattern,
+        fillColor: fillColor,
+        fillPattern: fillPattern,
+        strokeColor: strokeColor,
+        strokeWidthPx: strokeWidthPx);
+    canvas.drawImage(image, Point(bounds.left - image.width / 2, bounds.top - image.height / 2));
+  }
+}
