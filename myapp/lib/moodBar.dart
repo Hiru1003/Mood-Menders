bf170f070551371030720b2c8e396f02e1e26908
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-19 20:09:43 +0530

    modify: moodbar class

--- myapp/lib/moodBar.dart
+++ myapp/lib/moodBar.dart
@@ -1,2 +1,19 @@
 //import 'package:flutter/material.dart';
 //import 'package:app/percentageBar.dart';
+
+import 'package:flutter/material.dart';
+
+class MoodImages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
