7fc9b833ded57b9469ab64be2c2f33f0438ad0cc
Hiru1003 <hirumiperera10@gmail.com>
2024-02-19 09:18:41 +0530

    style: scaffold step 5

---
+++ myapp/lib/lovemedstep5.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class lovemedstep5_page extends StatefulWidget {
+  const lovemedstep5_page({
+    super.key,
+  });
+
+  @override
+  State<lovemedstep5_page> createState() => _lovemedstep5_pageState();
+}
+
+class _lovemedstep5_pageState extends State<lovemedstep5_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
