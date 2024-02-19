43d3c5e496bc6405d5a3fc8d144c7859e04dd3fe
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-19 21:39:05 +0530

    modify: dates format

--- myapp/lib/dates.dart
+++ myapp/lib/dates.dart
@@ -4,29 +4,29 @@ class DatesWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     DateTime today = DateTime.now();
-    final controller = ScrollController(initialScrollOffset: 3 * 60.0); // Scroll to the middle of the week
+    final controller = ScrollController(initialScrollOffset: 3 * 60.0);
 
     return Container(
-      height: 100, // Increase the height to accommodate the bigger current day
+      height: 100,
       child: ListView.builder(
         controller: controller,
         scrollDirection: Axis.horizontal,
-        itemCount: 20, // Only show the current week
+        itemCount: 20,
         itemBuilder: (context, index) {
-          DateTime date = today.add(Duration(days: index - 3)); // Adjust the date to start from the beginning of the week
+          DateTime date = today.add(Duration(days: index - 3));
           bool isToday = date.day == today.day && date.month == today.month && date.year == today.year;
 
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: CircleAvatar(
-              radius: isToday ? 30 : 20, // Increase the radius for the current day
+              radius: isToday ? 30 : 20,
               backgroundColor: const Color.fromARGB(255, 154, 191, 188),
               child: Text(
                 date.day.toString(),
                 style: TextStyle(
                   color: const Color.fromARGB(255, 14, 3, 3),
                   fontWeight: isToday ? FontWeight.bold : FontWeight.normal,
-                  fontSize: isToday ? 24 : 14, // Increase the font size for the current day
+                  fontSize: isToday ? 24 : 14,
                 ),
               ),
             ),
