e315b9c12732aff99744619e724e13479299f9df
Hiru1003 <hirumiperera10@gmail.com>
2024-02-19 10:16:02 +0530

    style:scaffolld step 8

---
+++ myapp/lib/lovemedstep8.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class lovemedstep8_page extends StatefulWidget {
+  const lovemedstep8_page({
+    super.key,
+  });
+
+  @override
+  State<lovemedstep8_page> createState() => _lovemedstep8_pageState();
+}
+
+class _lovemedstep8_pageState extends State<lovemedstep8_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
