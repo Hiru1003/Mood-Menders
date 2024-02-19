4edc2e11e7c873670151a209978ae23002e56b03
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-19 21:26:16 +0530

    modify: dates loop

--- myapp/lib/dates.dart
+++ myapp/lib/dates.dart
@@ -4,10 +4,12 @@ class DatesWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     DateTime today = DateTime.now();
+    final controller = ScrollController(initialScrollOffset: 500 * 60.0); // 60.0 is the approximate width of each item
 
     return Container(
-      height: 60,
+      height: 80, // Increase the height to accommodate the bigger current day
       child: ListView.builder(
+        controller: controller,
         scrollDirection: Axis.horizontal,
         itemCount: 1000,
         itemBuilder: (context, index) {
@@ -17,20 +19,20 @@ class DatesWidget extends StatelessWidget {
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: CircleAvatar(
-              radius: isToday ? 25 : 20,
+              radius: isToday ? 30 : 20, // Increase the radius for the current day
               backgroundColor: const Color.fromARGB(255, 154, 191, 188),
               child: Text(
                 date.day.toString(),
                 style: TextStyle(
                   color: const Color.fromARGB(255, 14, 3, 3),
                   fontWeight: isToday ? FontWeight.bold : FontWeight.normal,
-                  fontSize: isToday ? 18 : 14,
+                  fontSize: isToday ? 22 : 14, // Increase the font size for the current day
                 ),
               ),
             ),
           );
         },
-      )
+      ),
     );
   }
 }
