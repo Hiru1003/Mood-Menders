bd16a41fe6a2877014969e59f3194b08388819e8
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-19 21:32:59 +0530

    style: today

--- myapp/lib/dates.dart
+++ myapp/lib/dates.dart
@@ -19,7 +19,7 @@ class DatesWidget extends StatelessWidget {
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: CircleAvatar(
-              radius: isToday ? 35 : 20, // Increase the radius for the current day
+              radius: isToday ? 30 : 20, // Increase the radius for the current day
               backgroundColor: const Color.fromARGB(255, 154, 191, 188),
               child: Text(
                 date.day.toString(),
