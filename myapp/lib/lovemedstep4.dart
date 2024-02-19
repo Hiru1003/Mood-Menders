a3d521df40d989fa0edf9ddaad3140ae711f9a7a
Hiru1003 <hirumiperera10@gmail.com>
2024-02-19 09:08:22 +0530

    feat:navigation

---
+++ myapp/lib/lovemedstep4.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class lovemedstep4_page extends StatefulWidget {
+  const lovemedstep4_page({
+    super.key,
+  });
+
+  @override
+  State<lovemedstep4_page> createState() => _lovemedstep4_pageState();
+}
+
+class _lovemedstep4_pageState extends State<lovemedstep4_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
