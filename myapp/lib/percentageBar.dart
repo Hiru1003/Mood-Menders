228e32c3a22e989c372d5afaa3b9ed1e8db48eb0
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-19 20:40:59 +0530

    modiyf: percentagebar class

---
+++ myapp/lib/percentageBar.dart
@@ -0,0 +1,29 @@
+
+class PercentageBar extends StatelessWidget {
+  final double percentage;
+
+  PercentageBar({this.percentage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 10,
+      width: 200,
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: FractionallySizedBox(
+        alignment: Alignment.centerLeft,
+        widthFactor: percentage,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(5),
+          ),
+        ),
+      ),
+    );
+  }
+}
+```
