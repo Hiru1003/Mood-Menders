957624e4cdc29cc5e317256fba31c31741215f13
Hiru1003 <hirumiperera10@gmail.com>
2024-02-19 10:18:08 +0530

    style:step 10

--- myapp/lib/lovemedstep10.dart
+++ myapp/lib/lovemedstep10.dart
@@ -13,6 +13,45 @@ class lovemedstep10_page extends StatefulWidget {
 class _lovemedstep10_pageState extends State<lovemedstep10_page> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(
+            left: 10,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'LOVING-KINDNESS \nMEDITATION',
+                style: GoogleFonts.poppins(
+                    textStyle: Theme.of(context).textTheme.displayLarge,
+                    color: const Color.fromARGB(255, 70, 66, 68),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500),
+              ),
+              Image.asset(
+                'lib/images/leaf.png',
+                height: 72,
+              )
+            ],
+          ),
+        ),
+        titleSpacing: 4.0,
+        toolbarHeight: 65,
+        toolbarOpacity: 0.9,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(18),
+              bottomLeft: Radius.circular(18)),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.black, //change your color here
+        ),
+        elevation: 0.00,
+        backgroundColor: Color.fromARGB(255, 134, 208, 203),
+      ),
+    );
   }
 }
