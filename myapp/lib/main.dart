66186f42e098e68f75675f4bc3aa5d659793a4a5
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-19 21:03:17 +0530

    modify: main

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app/moodBar.dart';
+import 'package:app/diary.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false, 
         //home: loading_page()
-        home: MoodImages(),
+        home: Diary(),
         );
   }
 }
