8d1b83073f01b0cde496205d3c909262377bd5c3
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-19 20:34:57 +0530

    modify: children buildrow

--- myapp/lib/moodBar.dart
+++ myapp/lib/moodBar.dart
@@ -4,6 +4,13 @@ class MoodImages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      children: [
+        buildRow('lib/images/loveearth (1).png', 0.2),
+        buildRow('lib/images/loveearth (2).png', 0.4),
+        buildRow('lib/images/loveearth (3).png', 0.3),
+        buildRow('lib/images/loveearth (4).png', 0.6),
+        buildRow('lib/images/loveearth (5).png', 0.8),
+      ],
     );
   }
 }
