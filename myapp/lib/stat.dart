58f75a83cfae5843addde1463eea906ea3b71187
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-19 20:58:09 +0530

    modify: stat format

--- myapp/lib/stat.dart
+++ myapp/lib/stat.dart
@@ -18,23 +18,23 @@ class _StatState extends State<Stat> {
 
   static List<charts.Series<LinearMood, int>> _createSampleData() {
     final data = [
-      new LinearMood(0, 5),
-      new LinearMood(1, 25),
-      new LinearMood(2, 100),
-      new LinearMood(3, 75),
-      new LinearMood(4, 50),
-      new LinearMood(5, 85),
-      new LinearMood(6, 40),
+      LinearMood(0, 5),
+      LinearMood(1, 25),
+      LinearMood(2, 100),
+      LinearMood(3, 75),
+      LinearMood(4, 50),
+      LinearMood(5, 85),
+      LinearMood(6, 40),
     ];
 
     return [
-      new charts.Series<LinearMood, int>(
+      charts.Series<LinearMood, int>(
         id: 'Mood',
         colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
         areaColorFn: (_, __) =>
             charts.MaterialPalette.blue.shadeDefault.lighter,
-        domainFn: (LinearMood Mood, _) => Mood.year,
-        measureFn: (LinearMood Mood, _) => Mood.Mood,
+        domainFn: (LinearMood mood, _) => mood.year,
+        measureFn: (LinearMood mood, _) => mood.mood,
         data: data,
       )
     ];
@@ -43,63 +43,63 @@ class _StatState extends State<Stat> {
   @override
   void initState() {
     super.initState();
-      startOfWeek = now.subtract(Duration(days: now.weekday - 1));
-    }
+    startOfWeek = now.subtract(Duration(days: now.weekday - 1));
+  }
 
-    @override
-    Widget build(BuildContext context) {
-      String formattedStartOfWeek = DateFormat('MMM d').format(startOfWeek);
-      String formattedToday = DateFormat('MMM d').format(now);
+  @override
+  Widget build(BuildContext context) {
+    String formattedStartOfWeek = DateFormat('MMM d').format(startOfWeek);
+    String formattedToday = DateFormat('MMM d').format(now);
 
-      return Scaffold(
-        appBar: AppBar(
-          title: Padding(
-            padding: const EdgeInsets.only(
-              left: 10,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 5.0, top:20.0), 
-                      child: Text(
-                        'TRACK \nYOUR MOODS',
-                        style: GoogleFonts.poppins(
-                          textStyle: Theme.of(context).textTheme.headlineMedium,
-                          color: const Color.fromARGB(255, 70, 66, 68),
-                          fontSize: 24,
-                          fontWeight: FontWeight.w600,
-                        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(
+            left: 10,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5.0, top: 20.0),
+                    child: Text(
+                      'TRACK \nYOUR MOODS',
+                      style: GoogleFonts.poppins(
+                        textStyle: Theme.of(context).textTheme.headlineMedium,
+                        color: const Color.fromARGB(255, 70, 66, 68),
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
-                  ],
-                ),
-                Image.asset(
-                  'lib/images/statMainpic.png',
-                  fit: BoxFit.cover,
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+              Image.asset(
+                'lib/images/statMainpic.png',
+                fit: BoxFit.cover,
+              ),
+            ],
           ),
-          titleSpacing: 4.0,
-          toolbarHeight: 130,
-          toolbarOpacity: 0.9,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(18),
-                bottomLeft: Radius.circular(18)),
-          ),
-          iconTheme: const IconThemeData(
-            color: Colors.black,
-          ),
-          elevation: 0.00,
-          backgroundColor: const Color.fromARGB(255, 134, 208, 203),
         ),
-        backgroundColor: const Color.fromARGB(251, 241, 255, 252),
-        body: SingleChildScrollView(
+        titleSpacing: 4.0,
+        toolbarHeight: 130,
+        toolbarOpacity: 0.9,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(18),
+              bottomLeft: Radius.circular(18)),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+        elevation: 0.00,
+        backgroundColor: const Color.fromARGB(255, 134, 208, 203),
+      ),
+      backgroundColor: const Color.fromARGB(251, 241, 255, 252),
+      body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
             Container(
@@ -122,7 +122,8 @@ class _StatState extends State<Stat> {
                           onTap: () {
                             setState(() {
                               now = now.subtract(const Duration(days: 7));
-                              startOfWeek = startOfWeek.subtract(const Duration(days: 7));
+                              startOfWeek =
+                                  startOfWeek.subtract(const Duration(days: 7));
                             });
                           },
                           child: const Icon(Icons.arrow_left),
@@ -138,7 +139,8 @@ class _StatState extends State<Stat> {
                           onTap: () {
                             setState(() {
                               now = now.add(const Duration(days: 7));
-                              startOfWeek = startOfWeek.add(const Duration(days: 7));
+                              startOfWeek =
+                                  startOfWeek.add(const Duration(days: 7));
                             });
                           },
                           child: const Icon(Icons.arrow_right),
@@ -147,55 +149,51 @@ class _StatState extends State<Stat> {
                     ),
                   ),
                   Expanded(
-    child: charts.LineChart(
-      seriesList as List<charts.Series<dynamic, num>>,
-      defaultRenderer: charts.LineRendererConfig(includeArea: true, stacked: true),
-      primaryMeasureAxis: const charts.NumericAxisSpec(
-        renderSpec: charts.NoneRenderSpec(),
-      ),
-      domainAxis: charts.NumericAxisSpec(
-    tickProviderSpec: charts.StaticNumericTickProviderSpec(
-    List.generate(7, (index) {
-      return new charts.TickSpec(
-        index,
-        label: ['S', 'M', 'T', 'W', 'T', 'F', 'S'][index],
-        style: new charts.TextStyleSpec(
-          fontSize: 13, 
-          color: charts.MaterialPalette.black,
-        ),
-      );
-    }),
-  ),
-  renderSpec: const charts.GridlineRendererSpec(
-    labelStyle: charts.TextStyleSpec(
-      fontSize: 0,
-      color: charts.MaterialPalette.transparent, 
-    ),
-    lineStyle: charts.LineStyleSpec(
-      color: charts.MaterialPalette.transparent, 
-    ),
-  ),
-),
-    ),
-  ),
-  
-              ],
-              
+                    child: charts.LineChart(
+                      seriesList as List<charts.Series<dynamic, num>>,
+                      defaultRenderer: charts.LineRendererConfig(
+                          includeArea: true, stacked: true),
+                      primaryMeasureAxis: const charts.NumericAxisSpec(
+                        renderSpec: charts.NoneRenderSpec(),
+                      ),
+                      domainAxis: charts.NumericAxisSpec(
+                        tickProviderSpec: charts.StaticNumericTickProviderSpec(
+                          List.generate(7, (index) {
+                            return charts.TickSpec(
+                              index,
+                              label: ['S', 'M', 'T', 'W', 'T', 'F', 'S'][index],
+                              style: charts.TextStyleSpec(
+                                fontSize: 13,
+                                color: charts.MaterialPalette.black,
+                              ),
+                            );
+                          }),
+                        ),
+                        renderSpec: const charts.GridlineRendererSpec(
+                          labelStyle: charts.TextStyleSpec(
+                            fontSize: 0,
+                            color: charts.MaterialPalette.transparent,
+                          ),
+                          lineStyle: charts.LineStyleSpec(
+                            color: charts.MaterialPalette.transparent,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-      ),
-     
-      ],
-    ),
+          ],
         ),
-      );
-
-
+      ),
+    );
   }
 }
 
 class LinearMood {
   final int year;
-  final int Mood;
+  final int mood;
 
-  LinearMood(this.year, this.Mood);
+  LinearMood(this.year, this.mood);
 }
