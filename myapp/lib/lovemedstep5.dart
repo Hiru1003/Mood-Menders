24301fbb702ce702b991f2e1698b671aa87c46ab
Hiru1003 <hirumiperera10@gmail.com>
2024-02-19 09:19:32 +0530

    style:logo

--- myapp/lib/lovemedstep5.dart
+++ myapp/lib/lovemedstep5.dart
@@ -32,7 +32,7 @@ class _lovemedstep5_pageState extends State<lovemedstep5_page> {
                     fontWeight: FontWeight.w400),
               ),
               Image.asset(
-                'lib/images/bg.png',
+                'lib/images/leaf.png',
                 height: 72,
               )
             ],
