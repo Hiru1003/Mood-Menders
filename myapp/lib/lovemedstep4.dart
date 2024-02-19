b6e3112879aa733e53dcbcf04cce79df9734935a
Hiru1003 <hirumiperera10@gmail.com>
2024-02-19 09:12:22 +0530

    style:button

--- myapp/lib/lovemedstep4.dart
+++ myapp/lib/lovemedstep4.dart
@@ -1,3 +1,4 @@
+import 'package:app/loving-kindnessmed.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -127,6 +128,25 @@ class _lovemedstep4_pageState extends State<lovemedstep4_page> {
                   ],
                 ),
               ),
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          //continue button
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: MyButton(
+              buttontext: 'Next Step',
+              onTap: () {
+                {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => (const lovekindmed_page())),
+                  );
+                }
+              },
             ),
           ),
         ])));
