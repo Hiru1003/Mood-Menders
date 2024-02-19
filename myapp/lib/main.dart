faa9976b24d9af9399cffc5ae5a2de780e6cc92f
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-19 21:59:46 +0530

    modify: main -> moodiamge

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app/percentageBar.dart';
+import 'package:app/moodBar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(debugShowCheckedModeBanner: false,
         //home: loading_page()
-        home: PercentageBar(percentage: 4,),
+        home: MoodImages(),
         );
   }
 }
