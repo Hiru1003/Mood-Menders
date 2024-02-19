b082c9effa0665af5aa691aa48fe7020a1ed448c
Hiru1003 <hirumiperera10@gmail.com>
2024-02-19 09:02:29 +0530

    style:column

--- myapp/lib/lovemedstep3.dart
+++ myapp/lib/lovemedstep3.dart
@@ -53,6 +53,6 @@ class _lovemedstep3_pageState extends State<lovemedstep3_page> {
           backgroundColor: Color.fromARGB(255, 134, 208, 203),
         ),
         backgroundColor: const Color.fromARGB(251, 241, 255, 252),
-        body: SingleChildScrollView());
+        body: SingleChildScrollView(child: Column(children: [])));
   }
 }
