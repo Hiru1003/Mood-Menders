2c4a0fea9564f6c9b5fde0cfab64f7c20bf59ccb
Hiru1003 <hirumiperera10@gmail.com>
2024-02-19 09:11:58 +0530

    style:word spacing

--- myapp/lib/lovemedstep4.dart
+++ myapp/lib/lovemedstep4.dart
@@ -120,6 +120,8 @@ class _lovemedstep4_pageState extends State<lovemedstep4_page> {
                           textStyle: Theme.of(context).textTheme.displayLarge,
                           color: const Color.fromARGB(255, 70, 66, 68),
                           fontSize: 13,
+                          height: 1.2,
+                          wordSpacing: 1.8,
                           fontWeight: FontWeight.w400),
                     ),
                   ],
