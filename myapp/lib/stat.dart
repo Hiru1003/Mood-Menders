04b40b404e7f52d8576fd20180a197fe923438b3
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-19 08:17:40 +0530

    style: padding 2

--- myapp/lib/stat.dart
+++ myapp/lib/stat.dart
@@ -244,9 +244,12 @@ Padding(
         'lib/images/loveearth (4).png',
         width: 50,
       ),
-      Text(
-        'Your text here', // replace with your text
-        style: TextStyle(fontSize: 16),
+      Padding(
+        padding: EdgeInsets.only(left: 20.0),
+        child: Text(
+          'Your text here', // replace with your text
+          style: TextStyle(fontSize: 16),
+        ),
       ),
     ],
   ),
