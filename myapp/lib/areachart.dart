e19926df23ea0ee9f44afb849d953681f0f1bb8b
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-19 22:12:08 +0530

    chart series

--- myapp/lib/areachart.dart
+++ myapp/lib/areachart.dart
@@ -4,3 +4,27 @@ class LinearMood {
 
   LinearMood(this.year, this.mood);
 }
+
+List<charts.Series<LinearMood, int>> _createSampleData() {
+  final data = [
+    LinearMood(0, 5),
+    LinearMood(1, 25),
+    LinearMood(2, 100),
+    LinearMood(3, 75),
+    LinearMood(4, 50),
+    LinearMood(5, 85),
+    LinearMood(6, 40),
+  ];
+
+  return [
+    charts.Series<LinearMood, int>(
+      id: 'Mood',
+      colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+      areaColorFn: (_, __) =>
+          charts.MaterialPalette.blue.shadeDefault.lighter,
+      domainFn: (LinearMood mood, _) => mood.year,
+      measureFn: (LinearMood mood, _) => mood.mood,
+      data: data,
+    )
+  ];
+}
