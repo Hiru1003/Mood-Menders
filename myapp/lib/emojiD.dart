67211eef5580e6ce858d05e218e8c7827ce1638b
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-20 14:11:44 +0530

    modify: emoji 2

--- myapp/lib/emojiD.dart
+++ myapp/lib/emojiD.dart
@@ -20,6 +20,17 @@ class ImageDisplay extends StatelessWidget {
             ),
           ),
         ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                recordEvent('lib/images/loveearth (2).png');
+              },
+              child: Image.asset('lib/images/loveearth (2).png', fit: BoxFit.scaleDown),
+            ),
+          ),
+        ),
         // ... Repeat for other images ...
       ],
     );
