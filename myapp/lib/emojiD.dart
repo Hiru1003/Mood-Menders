4d30725404cfb0d7ee3b1fb5f76f5fd433de28ca
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-20 14:10:58 +0530

    modify: emoji 1

---
+++ myapp/lib/emojiD.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ImageDisplay extends StatelessWidget {
+  final Function recordEvent;
+
+  ImageDisplay({required this.recordEvent});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                recordEvent('lib/images/loveearth (1).png');
+              },
+              child: Image.asset('lib/images/loveearth (1).png', fit: BoxFit.scaleDown),
+            ),
+          ),
+        ),
+        // ... Repeat for other images ...
+      ],
+    );
+  }
+}
