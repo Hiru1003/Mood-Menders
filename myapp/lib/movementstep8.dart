7728beb95281cf63ac3af190d6a9c1494e94d6e4
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 12:47:40 +0530

    style:scaffold step 8

---
+++ myapp/lib/movementstep8.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class movementstep8_page extends StatefulWidget {
+  const movementstep8_page({
+    super.key,
+  });
+
+  @override
+  State<movementstep8_page> createState() => _movementstep8_pageState();
+}
+
+class _movementstep8_pageState extends State<movementstep8_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
