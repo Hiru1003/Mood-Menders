0151186ac31a74ec26329b561fc1b33f03c88b3a
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 15:51:31 +0530

    style:button

--- myapp/lib/movementstep6.dart
+++ myapp/lib/movementstep6.dart
@@ -130,6 +130,25 @@ class _movementstep6_pageState extends State<movementstep6_page> {
               ),
             ),
           ),
+          const SizedBox(
+            height: 25,
+          ),
+          //continue button
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: MyButton(
+              buttontext: 'Next Step',
+              onTap: () {
+                {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => (movementstep7_page())),
+                  );
+                }
+              },
+            ),
+          ),
         ])));
   }
 }
