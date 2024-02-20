55611143e600c252cf124310bb6bf01910bfd02e
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-20 10:50:24 +0530

    modify: sizedbox

--- myapp/lib/chat.dart
+++ myapp/lib/chat.dart
@@ -19,6 +19,7 @@ class Chat extends StatelessWidget {
           padding: const EdgeInsets.only(left: 10),
           child: Row(children: [
             Image.asset('lib/images/loveearth (1).png', width: 40,),
+            const SizedBox(width: 10),
             Text(
                 name,
                 style: GoogleFonts.poppins(
