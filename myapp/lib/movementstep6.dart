6724d7be710dc1ec9d9456e76712e9094d5337e3
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 15:42:11 +0530

    style:column

--- myapp/lib/movementstep6.dart
+++ myapp/lib/movementstep6.dart
@@ -32,7 +32,7 @@ class _movementstep6_pageState extends State<movementstep6_page> {
                     fontWeight: FontWeight.w400),
               ),
               Image.asset(
-                'lib/images/newbg.png',
+                'lib/images/leaf.png',
                 height: 72,
               )
             ],
