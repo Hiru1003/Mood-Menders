d0cb0c8d86e34d3f3014121d46d6078b74f590e3
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 12:41:41 +0530

    style:spacing

--- myapp/lib/movementstep4.dart
+++ myapp/lib/movementstep4.dart
@@ -121,6 +121,8 @@ class _movementstep4_pageState extends State<movementstep4_page> {
                           textStyle: Theme.of(context).textTheme.displayLarge,
                           color: const Color.fromARGB(255, 70, 66, 68),
                           fontSize: 13,
+                          height: 1.2,
+                          wordSpacing: 1.8,
                           fontWeight: FontWeight.w400),
                     ),
                   ],
