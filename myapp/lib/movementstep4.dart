e002809fbadfb7b04adb3d831e3451d11d179989
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 12:36:37 +0530

    style:appabar

--- myapp/lib/movementstep4.dart
+++ myapp/lib/movementstep4.dart
@@ -13,6 +13,25 @@ class movementstep4_page extends StatefulWidget {
 class _movementstep4_pageState extends State<movementstep4_page> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+      title: Padding(
+        padding: const EdgeInsets.only(left: 10,),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+        Text('MOVEMENT-BASED \nMEDITATION',style: GoogleFonts.poppins(
+          textStyle: Theme.of(context).textTheme.displayLarge,
+          color: const Color.fromARGB(255, 70, 66, 68),
+          fontSize: 18,
+          fontWeight: FontWeight.w400
+          ), ),
+          Image.asset('lib/images/newbg.png',
+          height: 72,)
+          ],
+          ),
+      ),
+    );
   }
 }
