c172c3e50244725c954aa3b148fe9040b521aae0
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 12:43:30 +0530

    style:scaffold step 5

---
+++ myapp/lib/movementstep5.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class movementstep5_page extends StatefulWidget {
+  const movementstep5_page({
+    super.key,
+  });
+
+  @override
+  State<movementstep5_page> createState() => _movementstep5_pageState();
+}
+
+class _movementstep5_pageState extends State<movementstep5_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
