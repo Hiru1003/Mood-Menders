b2fd23b5c49a68e51d91977929b79009243b0a28
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 12:48:12 +0530

    style:create scaffold step 9

---
+++ myapp/lib/movementstep9.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class movementstep9_page extends StatefulWidget {
+  const movementstep9_page({
+    super.key,
+  });
+
+  @override
+  State<movementstep9_page> createState() => _movementstep9_pageState();
+}
+
+class _movementstep9_pageState extends State<movementstep9_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
