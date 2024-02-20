5f6e9faa50984620177c7230df128e05927c955c
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 08:11:07 +0530

    style: bg color

--- myapp/lib/movementstep3.dart
+++ myapp/lib/movementstep3.dart
@@ -14,44 +14,45 @@ class _movementstep3_pageState extends State<movementstep3_page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(
-            left: 10,
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'MOVEMENT-BASED \nMEDITATION',
+                  style: GoogleFonts.poppins(
+                      textStyle: Theme.of(context).textTheme.displayLarge,
+                      color: const Color.fromARGB(255, 70, 66, 68),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400),
+                ),
+                Image.asset(
+                  'lib/images/leaf.png',
+                  height: 72,
+                )
+              ],
+            ),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'MOVEMENT-BASED \nMEDITATION',
-                style: GoogleFonts.poppins(
-                    textStyle: Theme.of(context).textTheme.displayLarge,
-                    color: const Color.fromARGB(255, 70, 66, 68),
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400),
-              ),
-              Image.asset(
-                'lib/images/leaf.png',
-                height: 72,
-              )
-            ],
+          titleSpacing: 4.0,
+          toolbarHeight: 65,
+          toolbarOpacity: 0.9,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(18),
+                bottomLeft: Radius.circular(18)),
           ),
+          iconTheme: const IconThemeData(
+            color: Colors.black, //change your color here
+          ),
+          elevation: 0.00,
+          backgroundColor: Color.fromARGB(255, 134, 208, 203),
         ),
-        titleSpacing: 4.0,
-        toolbarHeight: 65,
-        toolbarOpacity: 0.9,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(18),
-              bottomLeft: Radius.circular(18)),
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.black, //change your color here
-        ),
-        elevation: 0.00,
-        backgroundColor: Color.fromARGB(255, 134, 208, 203),
-      ),
-    );
+        backgroundColor: const Color.fromARGB(251, 241, 255, 252),
+        body: SingleChildScrollView());
   }
 }
