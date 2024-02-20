e8f28f41305e375510d0264fe7f15532c0e024e8
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-20 11:21:55 +0530

    modify: text

--- myapp/lib/chat.dart
+++ myapp/lib/chat.dart
@@ -60,6 +60,7 @@ class Chat extends StatelessWidget {
                   fontWeight: FontWeight.w600,),
                 ),
               ),
+              Text('Lorem ipsum dollar')
             ],
           ),
         ),
