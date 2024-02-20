75c5924f8d35b42029984cb12c3a26323578ba73
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 15:43:35 +0530

    modify:font weight

--- myapp/lib/movementstep5.dart
+++ myapp/lib/movementstep5.dart
@@ -29,7 +29,7 @@ class _movementstep5_pageState extends State<movementstep5_page> {
                       textStyle: Theme.of(context).textTheme.displayLarge,
                       color: const Color.fromARGB(255, 70, 66, 68),
                       fontSize: 18,
-                      fontWeight: FontWeight.w400),
+                      fontWeight: FontWeight.w500),
                 ),
                 Image.asset(
                   'lib/images/leaf.png',
