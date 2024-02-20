b2a6f1ce6252f2c492808a0d0e05babf48385fd5
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-20 17:57:21 +0530

    revert: newdiary

--- myapp/lib/newDiary.dart
+++ myapp/lib/newDiary.dart
@@ -1,29 +1,23 @@
 import 'package:app/appBarD.dart';
 import 'package:app/emojiD.dart';
+import 'package:app/gradientButton.dart';
+import 'package:app/newDiary2.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class NewDiary extends StatefulWidget {
+class NewDiary extends StatelessWidget {
   const NewDiary({Key? key}) : super(key: key);
-
-  @override
-  _NewDiaryState createState() => _NewDiaryState();
-}
-
-class _NewDiaryState extends State<NewDiary> {
-  String selectedButton = '';
 
   void recordEvent(String imagePath) {
     print('Image at $imagePath was tapped');
     // Add your logic for recording the event here
+  
   }
 
-  void handleButtonPress(String buttonText) {
-    setState(() {
-      selectedButton = buttonText;
-    });
+   void handleButtonPress(String buttonText) {
     print('Button $buttonText was pressed');
     // Add your logic for handling button presses here
+
   }
 
   @override
@@ -75,24 +69,21 @@ class _NewDiaryState extends State<NewDiary> {
                       Padding(
                         padding: const EdgeInsets.all(3.0),
                         child: ElevatedButton(
-                          onPressed: () => handleButtonPress('Proud'),
-                          style: ElevatedButton.styleFrom(
-                            primary: selectedButton == 'Proud'
-                                ? Colors.green // Change the color for the selected button
-                                : null,
-                          ),
+                          onPressed: () {},
+                          child: const Text('Excited'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
                           child: const Text('Proud'),
                         ),
                       ),
                       Padding(
                         padding: const EdgeInsets.all(3.0),
                         child: ElevatedButton(
-                          onPressed: () => handleButtonPress('Lonely'),
-                          style: ElevatedButton.styleFrom(
-                            primary: selectedButton == 'Lonely'
-                                ? Colors.green // Change the color for the selected button
-                                : null,
-                          ),
+                          onPressed: () {},
                           child: const Text('Lonely'),
                         ),
                       ),
@@ -104,40 +95,129 @@ class _NewDiaryState extends State<NewDiary> {
                       Padding(
                         padding: const EdgeInsets.all(3),
                         child: ElevatedButton(
-                          onPressed: () => handleButtonPress('Stressed'),
-                          style: ElevatedButton.styleFrom(
-                            primary: selectedButton == 'Stressed'
-                                ? Colors.green // Change the color for the selected button
-                                : null,
-                          ),
+                          onPressed: () {},
                           child: const Text('Stressed'),
                         ),
                       ),
                       Padding(
                         padding: const EdgeInsets.all(3),
                         child: ElevatedButton(
-                          onPressed: () => handleButtonPress('Tired'),
-                          style: ElevatedButton.styleFrom(
-                            primary: selectedButton == 'Tired'
-                                ? Colors.green // Change the color for the selected button
-                                : null,
-                          ),
+                          onPressed: () {},
                           child: const Text('Tired'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(3),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Annoyed'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(3),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Happy'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(3),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Angry'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(3),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Joyful'),
                         ),
                       ),
                     ],
                   ),
                   Padding(
-                    padding: const EdgeInsets.all(3.0),
-                    child: ElevatedButton(
-                      onPressed: () => handleButtonPress('Excited'),
-                      style: ElevatedButton.styleFrom(
-                        primary: selectedButton == 'Excited'
-                            ? Colors.green // Change the color for the selected button
-                            : null,
+                    padding: const EdgeInsets.only(top: 15,),
+                    child: Text(
+                      'Sphere of life',
+                      style: GoogleFonts.poppins(
+                        textStyle: Theme.of(context).textTheme.headlineMedium,
+                        color: const Color.fromARGB(255, 70, 66, 68),
+                        fontSize: 24,
+                        fontWeight: FontWeight.w400,
                       ),
-                      child: const Text('Excited'),
                     ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 26.0, left: 26.0),
+                          child: Image.asset('lib/images/edu.png', fit: BoxFit.scaleDown),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(26.0),
+                          child: Image.asset('lib/images/love.png', fit: BoxFit.scaleDown),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(26.0),
+                          child: Image.asset('lib/images/famliy.png', fit: BoxFit.scaleDown),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(26.0),
+                          child: Image.asset('lib/images/health.png', fit: BoxFit.scaleDown),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(26.0),
+                          child: Image.asset('lib/images/personal.png', fit: BoxFit.scaleDown),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(26.0),
+                          child: Image.asset('lib/images/finance.png', fit: BoxFit.scaleDown),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(26.0),
+                          child: Image.asset('lib/images/friends.png', fit: BoxFit.scaleDown),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(26.0),
+                          child: Image.asset('lib/images/leisure.png', fit: BoxFit.scaleDown),
+                        ),
+                      ),
+                    ],
+                  ),
+                  GradientButton(
+                    buttonText: 'Continue',
+                    enableIcon: true,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => NewDiary2()),
+                      );
+                    },
                   ),
                 ],
               ),
