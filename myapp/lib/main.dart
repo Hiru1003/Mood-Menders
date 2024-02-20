e1461bd3ac97970a37ea174d7c1b8ea171864675
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-20 17:30:03 +0530

    modify: add name

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false,
         //home: loading_page()
-        home: Chat(),
+        home: Chat('Risini'),
         );
   }
 }
