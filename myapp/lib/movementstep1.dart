a2c12f88bd142d969b846dc1ac20fbeef7cf230e
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 06:45:10 +0530

    style:container

--- myapp/lib/movementstep1.dart
+++ myapp/lib/movementstep1.dart
@@ -107,6 +107,7 @@ class _movementstep1_pageState extends State<movementstep1_page> {
           //para
           Padding(
             padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Container(),
           ),
         ])));
   }
