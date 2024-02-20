51c2d734dac19e5b32dc0738de9e0acf5615b233
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 15:47:04 +0530

    style:height

--- myapp/lib/movementstep5.dart
+++ myapp/lib/movementstep5.dart
@@ -121,6 +121,8 @@ class _movementstep5_pageState extends State<movementstep5_page> {
                           textStyle: Theme.of(context).textTheme.displayLarge,
                           color: const Color.fromARGB(255, 70, 66, 68),
                           fontSize: 13,
+                          height: 1.2,
+                          wordSpacing: 1.8,
                           fontWeight: FontWeight.w400),
                     ),
                   ],
