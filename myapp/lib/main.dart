b0fcf9a3ca7f235367286b05d4170dcb280f03f4
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-20 22:44:13 +0530

    fix: conflicts

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app/newDiary.dart';
+import 'package:app/loading_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false,
-        //home: loading_page()
-        home: NewDiary(),
+    return MaterialApp(debugShowCheckedModeBanner: false, home: loading_page()
+        //home: Stat(),
         );
   }
 }
