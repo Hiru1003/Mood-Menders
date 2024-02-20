42da95a0e7aaaa8d1be0b43299b209b25a0905e7
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 15:39:01 +0530

    modify:logo

--- myapp/lib/movementstep5.dart
+++ myapp/lib/movementstep5.dart
@@ -32,7 +32,7 @@ class _movementstep5_pageState extends State<movementstep5_page> {
                     fontWeight: FontWeight.w400),
               ),
               Image.asset(
-                'lib/images/newbg.png',
+                'lib/images/leaf.png',
                 height: 72,
               )
             ],
