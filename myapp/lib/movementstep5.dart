5dd24cba81a7dd321e1820a8ef959b970ead9aac
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 15:46:03 +0530

    style:text

--- myapp/lib/movementstep5.dart
+++ myapp/lib/movementstep5.dart
@@ -85,7 +85,18 @@ class _movementstep5_pageState extends State<movementstep5_page> {
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [],
+                  children: [
+                    //message
+                    Text(
+                      'Step 05',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          color: const Color.fromARGB(255, 70, 66, 68),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
                 ),
               ),
             ),
