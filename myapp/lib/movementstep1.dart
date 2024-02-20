c492f91aae07192c97e99a80bc327dcce6918236
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 06:41:20 +0530

    style: scaffold

---
+++ myapp/lib/movementstep1.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class movementstep1_page extends StatefulWidget {
+  const movementstep1_page({
+    super.key,
+  });
+
+  @override
+  State<movementstep1_page> createState() => _movementstep1_pageState();
+}
+
+class _movementstep1_pageState extends State<movementstep1_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
