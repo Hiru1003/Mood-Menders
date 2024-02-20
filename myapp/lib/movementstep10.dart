0f6741ae4978fffc54a0709d1e1ed781fe07baba
Hiru1003 <hirumiperera10@gmail.com>
2024-02-20 12:48:40 +0530

    style:create scaffold step 10

---
+++ myapp/lib/movementstep10.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class movementstep10_page extends StatefulWidget {
+  const movementstep10_page({
+    super.key,
+  });
+
+  @override
+  State<movementstep10_page> createState() => _movementstep10_pageState();
+}
+
+class _movementstep10_pageState extends State<movementstep10_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
