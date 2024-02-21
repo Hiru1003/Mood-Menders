9066dc2ff637323f19ff60d6820a69536792afd9
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 09:42:19 +0530

    style:para padding

--- myapp/lib/walkingstep2.dart
+++ myapp/lib/walkingstep2.dart
@@ -101,6 +101,22 @@ class _walkingstep2_pageState extends State<walkingstep2_page> {
               ),
             ),
           ),
+          const SizedBox(
+            height: 5,
+          ),
+          //para
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [],
+                ),
+              ),
+            ),
+          ),
         ])));
   }
 }
