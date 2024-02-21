f525464ebf05eda7c890a4e9e3dfc54d9bf49012
Risini Amarathunga <137677342+Risini-33@users.noreply.github.com>
2024-02-21 19:00:22 +0530

    newdiary2

--- myapp/lib/newDiary2.dart
+++ myapp/lib/newDiary2.dart
@@ -1,6 +1,6 @@
 import 'package:app/diary/appBarD.dart';
 import 'package:app/diary/diary.dart';
-import 'package:app/gradientButton.dart';
+import 'package:app/diary/gradientButton.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
