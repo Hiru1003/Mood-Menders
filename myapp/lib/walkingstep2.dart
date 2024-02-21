50ac093dc05378911853102f79fbd03e4cfe8367
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 09:40:36 +0530

    style:height

--- myapp/lib/walkingstep2.dart
+++ myapp/lib/walkingstep2.dart
@@ -53,6 +53,12 @@ class _walkingstep2_pageState extends State<walkingstep2_page> {
           backgroundColor: Color.fromARGB(255, 134, 208, 203),
         ),
         backgroundColor: const Color.fromARGB(251, 241, 255, 252),
-        body: SingleChildScrollView(child: Column(children: [])));
+        body: SingleChildScrollView(
+            child: Column(children: [
+          //image
+          const SizedBox(
+            height: 20,
+          ),
+        ])));
   }
 }
