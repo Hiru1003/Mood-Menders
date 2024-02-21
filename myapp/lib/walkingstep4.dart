9a8d123fd462a37b3b80c65013ad20c12d1edf8c
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 09:49:11 +0530

    style:scaffold step 4

---
+++ myapp/lib/walkingstep4.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class walkingstep4_page extends StatefulWidget {
+  const walkingstep4_page({
+    super.key,
+  });
+
+  @override
+  State<walkingstep4_page> createState() => _walkingstep4_pageState();
+}
+
+class _walkingstep4_pageState extends State<walkingstep4_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
