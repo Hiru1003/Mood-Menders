82a601a587eb4b262fb74d3453be6fc2b32a7aa9
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 09:44:03 +0530

    style: scaffolld step 3

---
+++ myapp/lib/walkingstep3.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class walkingstep3_page extends StatefulWidget {
+  const walkingstep3_page({
+    super.key,
+  });
+
+  @override
+  State<walkingstep3_page> createState() => _walkingstep3_pageState();
+}
+
+class _walkingstep3_pageState extends State<walkingstep3_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
