92b0141925310178525fee4ba5d6de8cac04b4c3
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 09:47:29 +0530

    style:text

--- myapp/lib/walkingstep3.dart
+++ myapp/lib/walkingstep3.dart
@@ -78,6 +78,29 @@ class _walkingstep3_pageState extends State<walkingstep3_page> {
           const SizedBox(
             height: 15,
           ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //message
+                    Text(
+                      'Step 03',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          color: const Color.fromARGB(255, 70, 66, 68),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ])));
   }
 }
