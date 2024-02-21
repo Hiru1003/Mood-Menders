f3a2ee8ac3b5beca5372da83e1140fab081245e6
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 09:42:57 +0530

    style:container

--- myapp/lib/walkingstep2.dart
+++ myapp/lib/walkingstep2.dart
@@ -130,6 +130,9 @@ class _walkingstep2_pageState extends State<walkingstep2_page> {
               ),
             ),
           ),
+          const SizedBox(
+            height: 25,
+          ),
         ])));
   }
 }
