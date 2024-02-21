78931047dc9639c3b9cd0bc679424415e60cba3f
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 09:39:11 +0530

    style:logo

--- myapp/lib/walkingstep2.dart
+++ myapp/lib/walkingstep2.dart
@@ -32,7 +32,7 @@ class _walkingstep2_pageState extends State<walkingstep2_page> {
                     fontWeight: FontWeight.w400),
               ),
               Image.asset(
-                'lib/images/newbg.png',
+                'lib/images/leaf.png',
                 height: 72,
               )
             ],
