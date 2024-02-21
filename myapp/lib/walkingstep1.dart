267e874340b867ec1d17f3ae441b39e1f41d4c92
Hiru1003 <hirumiperera10@gmail.com>
2024-02-21 09:35:25 +0530

    style:padding

--- myapp/lib/walkingstep1.dart
+++ myapp/lib/walkingstep1.dart
@@ -108,7 +108,15 @@ class _walkingstep1_pageState extends State<walkingstep1_page> {
           //para
           Padding(
             padding: const EdgeInsets.only(left: 10, right: 10),
-            child: Container(),
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [],
+                ),
+              ),
+            ),
           ),
         ])));
   }
