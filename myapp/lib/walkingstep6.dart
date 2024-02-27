c1b9f966361a34baab1daeb1943b7b42196e95d2
Hiru1003 <hirumiperera10@gmail.com>
2024-02-27 08:41:39 +0530

    feat: scaffold

---
+++ myapp/lib/walkingstep6.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class walkingstep6_page extends StatefulWidget {
+  const walkingstep6_page({
+    super.key,
+  });
+
+  @override
+  State<walkingstep6_page> createState() => _walkingstep6_pageState();
+}
+
+class _walkingstep6_pageState extends State<walkingstep6_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
