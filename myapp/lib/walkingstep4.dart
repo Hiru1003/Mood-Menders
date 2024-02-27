0b7c4303beea190fe988c7836781b35cbec76bd6
Hiru1003 <hirumiperera10@gmail.com>
2024-02-27 08:38:49 +0530

    feat: walking step 4

--- myapp/lib/walkingstep4.dart
+++ myapp/lib/walkingstep4.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:moodmenders/Components/button.dart';
+import 'package:moodmenders/pages/walkingstep5.dart';
 
 class walkingstep4_page extends StatefulWidget {
   const walkingstep4_page({
@@ -13,6 +15,147 @@ class walkingstep4_page extends StatefulWidget {
 class _walkingstep4_pageState extends State<walkingstep4_page> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(
+            left: 10,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'WALKING MEDITATION',
+                style: GoogleFonts.poppins(
+                    textStyle: Theme.of(context).textTheme.displayLarge,
+                    color: const Color.fromARGB(255, 70, 66, 68),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400),
+              ),
+              Image.asset(
+                'lib/images/newbg.png',
+                height: 72,
+              )
+            ],
+          ),
+        ),
+        titleSpacing: 4.0,
+        toolbarHeight: 65,
+        toolbarOpacity: 0.9,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(18),
+              bottomLeft: Radius.circular(18)),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.black, //change your color here
+        ),
+        elevation: 0.00,
+        backgroundColor: Color.fromARGB(255, 134, 208, 203),
+      ),
+      backgroundColor: const Color.fromARGB(251, 241, 255, 252),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //image
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Container(
+                padding: const EdgeInsets.all(1.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //image
+                    Image.asset(
+                      'lib/images/step11.png',
+                      height: 210,
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            const SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //message
+                      Text(
+                        'Step 04',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                            textStyle: Theme.of(context).textTheme.displayLarge,
+                            color: const Color.fromARGB(255, 70, 66, 68),
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(
+              height: 5,
+            ),
+            //para
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(1.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //message
+                      Text(
+                        'Coordinate your breath with your steps. For \nexample, you might take one step for each inhale \nand one step for each exhale. Find a rhythm that \nfeels natural to you.',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                            textStyle: Theme.of(context).textTheme.displayLarge,
+                            color: const Color.fromARGB(255, 70, 66, 68),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+            //continue button
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: MyButton(
+                buttontext: 'Next Step',
+                onTap: () {
+                  {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => (walkingstep5_page())),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
