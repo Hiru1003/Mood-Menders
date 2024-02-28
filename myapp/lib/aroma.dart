0ed4094c3ec5e75a162fcd76fe71125039613ddc
Hiru1003 <hirumiperera10@gmail.com>
2024-02-28 07:51:46 +0530

    feat:scaffold

---
+++ myapp/lib/aroma.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Aromatherapy_page extends StatefulWidget {
+  const Aromatherapy_page({
+    super.key,
+  });
+
+  @override
+  State<Aromatherapy_page> createState() => _Aromatherapy_pageState();
+}
+
+class _Aromatherapy_pageState extends State<Aromatherapy_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
