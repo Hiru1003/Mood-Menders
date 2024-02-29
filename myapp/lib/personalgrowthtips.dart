e8ad68d2fc1b188bc3e2a00f25b25e61b3e62102
Hiru1003 <hirumiperera10@gmail.com>
2024-02-29 09:11:28 +0530

    feat: container 1

--- myapp/lib/personalgrowthtips.dart
+++ myapp/lib/personalgrowthtips.dart
@@ -15,44 +15,154 @@ class _personalgrowthtips_pageState extends State<personalgrowthtips_page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(
-            left: 10,
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'PERSONAL GROWTH TIPS',
+                  style: GoogleFonts.poppins(
+                      textStyle: Theme.of(context).textTheme.displayLarge,
+                      color: const Color.fromARGB(255, 70, 66, 68),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500),
+                ),
+                Image.asset(
+                  'lib/images/leaf.png',
+                  height: 72,
+                )
+              ],
+            ),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'PERSONAL GROWTH TIPS',
-                style: GoogleFonts.poppins(
-                    textStyle: Theme.of(context).textTheme.displayLarge,
-                    color: const Color.fromARGB(255, 70, 66, 68),
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500),
+          titleSpacing: 4.0,
+          toolbarHeight: 65,
+          toolbarOpacity: 0.9,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(18),
+                bottomLeft: Radius.circular(18)),
+          ),
+          iconTheme: const IconThemeData(
+            color: Colors.black, //change your color here
+          ),
+          elevation: 0.00,
+          backgroundColor: Color.fromARGB(255, 134, 208, 203),
+        ),
+        backgroundColor: const Color.fromARGB(251, 241, 255, 252),
+        body: SingleChildScrollView(
+            child: Column(children: [
+          //story 01
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 217, 235, 237),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16))),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10, top: 7),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    //message
+                    Text(
+                      'Practice Self-Reflection',
+                      style: GoogleFonts.poppins(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          color: const Color.fromARGB(255, 70, 66, 68),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Image.asset(
+                            'lib/images/6.png',
+                            height: 45,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              Image.asset(
-                'lib/images/leaf.png',
-                height: 72,
-              )
-            ],
+            ),
           ),
-        ),
-        titleSpacing: 4.0,
-        toolbarHeight: 65,
-        toolbarOpacity: 0.9,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(18),
-              bottomLeft: Radius.circular(18)),
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.black, //change your color here
-        ),
-        elevation: 0.00,
-        backgroundColor: Color.fromARGB(255, 134, 208, 203),
-      ),
-    );
+
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 217, 235, 237),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10, top: 7),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    //message
+                    Text(
+                      'Encourage users to regularly reflect on their \nexperiences, emotions, and goals. Provide \nprompts or journaling exercises to facilitate this \nprocess.',
+                      textAlign: TextAlign.justify,
+                      style: GoogleFonts.poppins(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          color: const Color.fromARGB(255, 70, 66, 68),
+                          fontSize: 12,
+                          height: 1.2,
+                          wordSpacing: 2.0,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 217, 235, 237),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(16),
+                    bottomRight: Radius.circular(16)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 194, 207, 190),
+                    blurRadius: 4,
+                    offset: Offset(0, 6), // Shadow position
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    //message
+                    Text(
+                      '',
+                      style: GoogleFonts.poppins(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          color: const Color.fromARGB(255, 70, 66, 68),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ])));
   }
 }
