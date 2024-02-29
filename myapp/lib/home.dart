a5658e322b35147ddc9c41cba4caa2b541fd923f
Hiru1003 <hirumiperera10@gmail.com>
2024-02-29 07:56:23 +0530

    feat: relaxation container

--- myapp/lib/home.dart
+++ myapp/lib/home.dart
@@ -3,6 +3,7 @@ import 'package:app/diary/diary.dart';
 import 'package:app/meditaionmethods.dart';
 import 'package:app/motivationalquotes.dart';
 import 'package:app/personalgrowthtips.dart';
+import 'package:app/relaxation.dart';
 import 'package:app/successstories.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -405,26 +406,85 @@ class _home_pageState extends State<home_page> {
               padding: const EdgeInsets.only(left: 10, right: 10),
               child: Container(
                 decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 204, 248, 245),
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(16),
-                        topRight: Radius.circular(16))),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      //message
-                      Text(
-                        'Relaxation',
-                        style: GoogleFonts.poppins(
-                            textStyle: Theme.of(context).textTheme.displayLarge,
-                            color: const Color.fromARGB(255, 70, 66, 68),
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400),
-                      ),
-                    ],
+                  color: Color.fromARGB(255, 204, 248, 245),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16),
+                    bottomLeft: Radius.circular(16),
+                    bottomRight: Radius.circular(16),
                   ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromARGB(255, 194, 207, 190),
+                      blurRadius: 4,
+                      offset: Offset(0, 6), // Shadow position
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Section 1 - Title
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // Title
+                          Text(
+                            'Relaxation',
+                            style: GoogleFonts.poppins(
+                              textStyle:
+                                  Theme.of(context).textTheme.displayLarge,
+                              color: const Color.fromARGB(255, 70, 66, 68),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // Section 2 - Paragraph
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // Paragraph
+                          Text(
+                            'Relaxing your mind with us with different \neasy ways. We will guide you to \ncalm yourself ',
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.poppins(
+                              textStyle:
+                                  Theme.of(context).textTheme.displayLarge,
+                              color: const Color.fromARGB(255, 70, 66, 68),
+                              fontSize: 13,
+                              height: 1.2,
+                              wordSpacing: 1.9,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // Section 3 - Continue Button
+                    Padding(
+                      padding: const EdgeInsets.all(1.0),
+                      child: MyConButton(
+                        buttontext: 'Continue',
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const relaxation_page(),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
