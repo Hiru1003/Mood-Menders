34a42b3c8e2d8f269ee2d17dc1d8d0a787c589fc
Hiru1003 <hirumiperera10@gmail.com>
2024-03-22 00:43:18 +0530

    feat: motivational quotes

--- myapp/lib/motivationalquotes.dart
+++ myapp/lib/motivationalquotes.dart
@@ -14,6 +14,226 @@ class motivationalquotes_page extends StatefulWidget {
 class _motivationalquotes_pageState extends State<motivationalquotes_page> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(
+            left: 10,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'MORTIVATIONAL \nQUOTES',
+                style: GoogleFonts.poppins(
+                    textStyle: Theme.of(context).textTheme.displayLarge,
+                    color: const Color.fromARGB(255, 70, 66, 68),
+                    fontSize: 19,
+                    fontWeight: FontWeight.w400),
+              ),
+              Image.asset(
+                'lib/images/bg.png',
+                height: 72,
+              )
+            ],
+          ),
+        ),
+        titleSpacing: 4.0,
+        toolbarHeight: 65,
+        toolbarOpacity: 0.9,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(18),
+              bottomLeft: Radius.circular(18)),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.black, //change your color here
+        ),
+        elevation: 0.00,
+        backgroundColor: Color.fromARGB(255, 134, 208, 203),
+      ),
+      backgroundColor: const Color.fromARGB(251, 241, 255, 252),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          //six boxes
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //image
+                    Image.asset(
+                      'lib/images/m1.png',
+                      height: 140,
+                      width: 140,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    //image
+                    Image.asset(
+                      'lib/images/m2.png',
+                      height: 140,
+                      width: 140,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //image
+                    Image.asset(
+                      'lib/images/m3.png',
+                      height: 140,
+                      width: 140,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    //image
+                    Image.asset(
+                      'lib/images/m4.png',
+                      height: 140,
+                      width: 140,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //image
+                    Image.asset(
+                      'lib/images/m2.png',
+                      height: 140,
+                      width: 140,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    //image
+                    Image.asset(
+                      'lib/images/m6.png',
+                      height: 140,
+                      width: 140,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //image
+                    Image.asset(
+                      'lib/images/m7.png',
+                      height: 140,
+                      width: 140,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    //image
+                    Image.asset(
+                      'lib/images/m8.png',
+                      height: 140,
+                      width: 140,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
+        ]),
+      ),
+    );
   }
 }
