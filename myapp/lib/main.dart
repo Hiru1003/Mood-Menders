561137b4dcd3040cb6ac09219300490befc74566
Hiru1003 <hirumiperera10@gmail.com>
2024-03-21 14:34:10 +0530

    feat: dairy title and description firebase

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:app/Notification.dart';
 import 'package:app/aroma.dart';
+import 'package:app/diary/diary.dart';
 import 'package:app/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
